748496eea41cfe27a5b7584d910ba653793f3742
mb <mb@clearmatics.com>
2018-11-06 15:26:28 +0000

    test9

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ DEPLOY_SET=(dev-autonity-01.yml dev-autonity-02.yml dev-autonity-03.yml dev-auto
 echo $AUTONITY_DEV_CA_CRT | base64 --decode -i > ${HOME}/ca.crt
 
 kubectl config set-cluster our-k8s-cluster --embed-certs=true --server=${AUTONITY_DEV_CLUSTER_ENDPOINT} --certificate-authority=${HOME}/ca.crt
-kubectl config set-credentials travis-default --token=$SA_TOKEN
+kubectl config set-credentials travis-default --token=$AUTONITY_DEV_USER_TOKEN
 kubectl config set-context travis --cluster=$CLUSTER_NAME --user=travis-default --namespace=default
 kubectl config use-context travis
 kubectl config current-context
