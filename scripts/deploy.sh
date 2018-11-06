f31d3c3640014226525a50c36c2ef74516a993a7
mb <mb@clearmatics.com>
2018-11-06 14:03:20 +0000

    removed fetch repo / ssh key placement

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,11 +7,6 @@ TIMER_VAR=1
 DEPLOY_SET=(dev-autonity-01.yml dev-autonity-02.yml dev-autonity-03.yml dev-autonity-04.yml dev-autonity-05.yml)
 
 echo $AUTONITY_DEV_CA_CRT | base64 --decode -i > ${HOME}/ca.crt
-mkdir ${HOME}/.ssh/ && echo $AUTONITY_GITHUB_YML_PRIV_KEY > ${HOME}/.ssh/id_rsa
-echo $AUTONITY_GITHUB_YML_PUB_KEY > ${HOME}/.ssh/id_rsa.pub
-chmod 400 ${HOME}/*
-
-git clone git@github.com:clearmatics/autonity-deployments.git ${HOME}/
 
 kubectl config set-cluster our-k8s-cluster --embed-certs=true --server=${AUTONITY_DEV_CLUSTER_ENDPOINT} --certificate-authority=${HOME}/ca.crt
 kubectl config set-credentials travis-default --token=$SA_TOKEN
@@ -22,7 +17,7 @@ kubectl config current-context
 for i in "${DEPLOY_SET[@]}"
 do
 	:
-	kubectl delete -f $i && sleep $TIMER_VAR && kubectl apply -f $i
+	kubectl apply -f $i
 done
 
 # function cleaner {
