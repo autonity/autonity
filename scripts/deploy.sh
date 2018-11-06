1bf5eb010a38f00fe62659a5892bd0812d89e455
mb <mb@clearmatics.com>
2018-11-06 14:34:31 +0000

    test5

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,24 +3,21 @@ set -o pipefail
 set -o errexit
 set -o nounset
 
-echo "executed deploy script"
+DEPLOY_SET=(dev-autonity-01.yml dev-autonity-02.yml dev-autonity-03.yml dev-autonity-04.yml dev-autonity-05.yml)
 
-# TIMER_VAR=1
-# DEPLOY_SET=(dev-autonity-01.yml dev-autonity-02.yml dev-autonity-03.yml dev-autonity-04.yml dev-autonity-05.yml)
+echo $AUTONITY_DEV_CA_CRT | base64 --decode -i > ${HOME}/ca.crt
 
-# echo $AUTONITY_DEV_CA_CRT | base64 --decode -i > ${HOME}/ca.crt
+kubectl config set-cluster our-k8s-cluster --embed-certs=true --server=${AUTONITY_DEV_CLUSTER_ENDPOINT} --certificate-authority=${HOME}/ca.crt
+kubectl config set-credentials travis-default --token=$SA_TOKEN
+kubectl config set-context travis --cluster=$CLUSTER_NAME --user=travis-default --namespace=default
+kubectl config use-context travis
+kubectl config current-context
 
-# kubectl config set-cluster our-k8s-cluster --embed-certs=true --server=${AUTONITY_DEV_CLUSTER_ENDPOINT} --certificate-authority=${HOME}/ca.crt
-# kubectl config set-credentials travis-default --token=$SA_TOKEN
-# kubectl config set-context travis --cluster=$CLUSTER_NAME --user=travis-default --namespace=default
-# kubectl config use-context travis
-# kubectl config current-context
-
-# for i in "${DEPLOY_SET[@]}"
-# do
-# 	:
-# 	kubectl apply -f $i
-# done
+for i in "${DEPLOY_SET[@]}"
+do
+	:
+	kubectl apply -f $i
+done
 
 # # function cleaner {
 # #     rm -rvf "${HOME}/*"
