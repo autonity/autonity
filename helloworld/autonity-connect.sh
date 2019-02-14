c2bfa3c67bdb23c0933094fa1355cd19b49fb9b2
Duarte Aragao <daragao@gmail.com>
2019-02-14 23:05:54 +0000

    Docker Hello World IBFT working

--- helloworld/autonity-connect.sh
+++ helloworld/autonity-connect.sh
@@ -32,18 +32,17 @@ done
 
 # unlock all the things! (addresses)
 # set coinbase address
-for i in 1 2 3 4 5
+for i in 1 2 3 4
 do
   IDX=$(($i - 1))
   ADDRESS="http://172.25.0.1$i:8545"
   UNLOCKED=$(autonity attach $ADDRESS --exec "personal.unlockAccount(eth.accounts[$IDX],'test')")
-  IS_COINBASE_SET=$(autonity attach $ADDRESS --exec "miner.setEtherbase(eth.accounts[$IDX])")
+  # IS_COINBASE_SET=$(autonity attach $ADDRESS --exec "miner.setEtherbase(eth.accounts[$IDX])")
   COINBASE=$(autonity attach $ADDRESS --exec "eth.coinbase")
-  echo "Node $i $ADDRESS Account: $COINBASE Coinbase: $IS_COINBASE_SET Unlocked: $UNLOCKED"
+  echo "Node $i $ADDRESS Account: $COINBASE Unlocked: $UNLOCKED"
   # mine all the blocks! (start mining node)
   IS_MINING=$(autonity attach $ADDRESS --exec "miner.start()")
   echo "Node $i is mining"
 done
-
 
 # for i in {1..5}; do ./autonity attach http://0.0.0.0:854$i --exec '[eth.coinbase, eth.getBlock("latest").number, eth.getBlock("latest").hash, eth.mining]'; done
