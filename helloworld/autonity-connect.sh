ac68dabb8268c6b252a588e258f734eeb12ec40b
Duarte Aragão <daragao@gmail.com>
2019-02-15 00:17:44 +0000

    Autonity Hello world (#45)
    * clique autonity hello world with a single sealer
    
    * connector node more verbose
    
    * added comments to scripts
    
    * added README for helloworld
    
    * added too long didnt read to readme
    
    * fix path of docker-compose
    
    * WIP: failing to accept seals
    
    * Docker Hello World IBFT working
    
    * changed readme of helloworld
    
    * updated the README file

---
+++ helloworld/autonity-connect.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# arrays were not cooperating in the docker container (busybox) shell
+ENODE_1=$(autonity attach http://172.25.0.11:8545 --exec "admin.nodeInfo.enode.slice(8,136)")
+ENODE_2=$(autonity attach http://172.25.0.12:8545 --exec "admin.nodeInfo.enode.slice(8,136)")
+ENODE_3=$(autonity attach http://172.25.0.13:8545 --exec "admin.nodeInfo.enode.slice(8,136)")
+ENODE_4=$(autonity attach http://172.25.0.14:8545 --exec "admin.nodeInfo.enode.slice(8,136)")
+ENODE_5=$(autonity attach http://172.25.0.15:8545 --exec "admin.nodeInfo.enode.slice(8,136)")
+
+ENODE_1="enode://$(echo "${ENODE_1//\"}")@172.25.0.11:30303"
+ENODE_2="enode://$(echo "${ENODE_2//\"}")@172.25.0.12:30303"
+ENODE_3="enode://$(echo "${ENODE_3//\"}")@172.25.0.13:30303"
+ENODE_4="enode://$(echo "${ENODE_4//\"}")@172.25.0.14:30303"
+ENODE_5="enode://$(echo "${ENODE_5//\"}")@172.25.0.15:30303"
+
+echo "eNode 1: $ENODE_1"
+echo "eNode 2: $ENODE_2"
+echo "eNode 3: $ENODE_3"
+echo "eNode 4: $ENODE_4"
+echo "eNode 5: $ENODE_5"
+
+# connect all the things! (connect nodes between them)
+for i in 1 2 3 4 5
+do
+  ADDRESS="http://172.25.0.1$i:8545"
+  $(autonity attach $ADDRESS --exec "admin.addPeer('$ENODE_1')")
+  $(autonity attach $ADDRESS --exec "admin.addPeer('$ENODE_2')")
+  $(autonity attach $ADDRESS --exec "admin.addPeer('$ENODE_3')")
+  $(autonity attach $ADDRESS --exec "admin.addPeer('$ENODE_4')")
+  $(autonity attach $ADDRESS --exec "admin.addPeer('$ENODE_5')")
+done
+
+# unlock all the things! (addresses)
+# set coinbase address
+for i in 1 2 3 4
+do
+  IDX=$(($i - 1))
+  ADDRESS="http://172.25.0.1$i:8545"
+  UNLOCKED=$(autonity attach $ADDRESS --exec "personal.unlockAccount(eth.accounts[$IDX],'test')")
+  # IS_COINBASE_SET=$(autonity attach $ADDRESS --exec "miner.setEtherbase(eth.accounts[$IDX])")
+  COINBASE=$(autonity attach $ADDRESS --exec "eth.coinbase")
+  echo "Node $i $ADDRESS Account: $COINBASE Unlocked: $UNLOCKED"
+  # mine all the blocks! (start mining node)
+  IS_MINING=$(autonity attach $ADDRESS --exec "miner.start()")
+  echo "Node $i is mining"
+done
+
+# for i in {1..5}; do ./autonity attach http://0.0.0.0:854$i --exec '[eth.coinbase, eth.getBlock("latest").number, eth.getBlock("latest").hash, eth.mining]'; done
---
+++ helloworld/autonity-start.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+AUTONITY=autonity
+
+DATADIR=autonity-data
+KEYSTORE=keystore
+NODEKEY=nodekey
+RPC_PORT=8545
+RPC_ADDR=$(awk 'END{print $1}' /etc/hosts)
+RPC_API="istanbul,clique,console,eth,web3,admin,debug,miner,personal,txpool,net"
+
+# init the data directory
+echo "Autonity INIT $RPC_ADDR"
+$AUTONITY init --datadir $DATADIR genesis.json
+
+# start the node with the keystore and nodekey
+echo "Autonity START"
+$AUTONITY \
+  --datadir $DATADIR \
+  --nodekey $NODEKEY \
+  --keystore $KEYSTORE \
+  --rpc \
+  --rpcaddr $RPC_ADDR \
+  --rpcport $RPC_PORT \
+  --rpcapi "$RPC_API" \
+  --rpccorsdomain "*" \
+  --syncmode "full" \
+  --minerthreads 1
+  --verbosity 4 --debug
