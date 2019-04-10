74d9014fd04cd31d9a6da50c01a9091575adf820
Evgeny Danienko <6655321@bk.ru>
2019-04-10 13:26:23 +0300

    add cache into Nodes type

--- core/types/nodes.go
+++ core/types/nodes.go
@@ -3,6 +3,7 @@ package types
 import (
 	"github.com/clearmatics/autonity/log"
 	"github.com/clearmatics/autonity/p2p/enode"
+	"sync"
 )
 
 type Nodes struct {
@@ -17,7 +18,7 @@ func NewNodes(strList []string, openNetwork bool) *Nodes {
 	}
 
 	for _, enodeStr := range strList {
-		newEnode, err := enode.ParseV4WithResolve(enodeStr)
+		newEnode, err := cache.Get(enodeStr, enode.ParseV4WithResolve)
 		if err != nil {
 			log.Error("Invalid whitelisted enode", "returned enode", enodeStr, "error", err.Error())
 
@@ -31,4 +32,42 @@ func NewNodes(strList []string, openNetwork bool) *Nodes {
 	}
 
 	return n
+}
+
+var cache = &domainCache{}
+
+const defaultTTL = 20
+
+type domainCache struct {
+	m map[string] *struct {
+		node *enode.Node
+		count int
+	}
+	sync.RWMutex
+}
+
+func (c *domainCache) Get(enodeStr string, getter func(string) (*enode.Node, error)) (*enode.Node, error) {
+	c.RLock()
+	node, ok := c.m[enodeStr]
+	c.RUnlock()
+
+	if !ok {
+		// could be slow, so mutex is used
+		n, err := getter(enodeStr)
+		if err != nil {
+			return nil, err
+		}
+		c.Lock()
+		node.node = n
+		c.Unlock()
+	}
+
+	c.Lock()
+	node.count++
+	if node.count >= defaultTTL {
+		// reset the cache if TTL is reached
+		c.m[enodeStr] = nil
+	}
+	c.Unlock()
+	return node.node, nil
 }
