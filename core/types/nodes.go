9fffe9e239c97a6fe2f00586708eecefac8f58ef
Evgeny Danienko <6655321@bk.ru>
2019-04-18 13:41:40 +0300

    parse enodes in concurrent way

--- core/types/nodes.go
+++ core/types/nodes.go
@@ -1,10 +1,12 @@
 package types
 
 import (
+	"sync"
+	"sync/atomic"
+	"time"
+
 	"github.com/clearmatics/autonity/log"
 	"github.com/clearmatics/autonity/p2p/enode"
-	"sync"
-	"time"
 )
 
 type Nodes struct {
@@ -12,36 +14,89 @@ type Nodes struct {
 	StrList []string
 }
 
-func NewNodes(strList []string, openNetwork bool) *Nodes {
-	n := &Nodes{
-		[]*enode.Node{},
-		[]string{},
-	}
+const (
+	maxParseTries     = 300
+	delayBetweenTries = time.Second
+	defaultTTL        = 20
+)
 
-	getEnode := enode.ParseV4WithResolve
-	if !openNetwork {
-		getEnode = enode.GetParseV4WithResolveMaxTry(1000, time.Second)
+func NewNodes(strList []string, openNetwork bool) *Nodes {
+	getEnode := getParseFunc(openNetwork)
+
+	idx := new(int32)
+	wg := sync.WaitGroup{}
+	errCh := make(chan error, len(strList))
+
+	n := &Nodes{
+		make([]*enode.Node, len(strList)),
+		make([]string, len(strList)),
 	}
 
 	for _, enodeStr := range strList {
-		newEnode, err := cache.Get(enodeStr, getEnode)
-		if err != nil {
-			log.Error("Invalid whitelisted enode", "returned enode", enodeStr, "error", err.Error())
+		wg.Add(1)
 
-			if !openNetwork {
-				panic(err)
+		go func(enodeStr string) {
+			log.Error("performing", "node", enodeStr)
+			newEnode, err := cache.Get(enodeStr, getEnode)
+			if err != nil {
+				errCh <- err
 			}
-		}
-		n.List = append(n.List, newEnode)
-		n.StrList = append(n.StrList, enodeStr)
+
+			currentIdx := atomic.AddInt32(idx, 1) - 1
+			n.List[currentIdx] = newEnode
+			n.StrList[currentIdx] = enodeStr
+
+			wg.Done()
+		}(enodeStr)
 	}
 
-	return n
+	wg.Wait()
+	close(errCh)
+
+	var errs []error
+	for err := range errCh {
+		errs = append(errs, err)
+	}
+
+	if len(errs) != 0 {
+		if !openNetwork {
+			panic(errs)
+		}
+		log.Error("enodes parse errors", "errs", errs)
+	}
+
+	return filterNodes(n, openNetwork)
 }
 
-var cache = &domainCache{m:make(map[string]resolvedNode)}
+func filterNodes(n *Nodes, openNetwork bool) *Nodes {
+	filtered := &Nodes{
+		make([]*enode.Node, 0, len(n.List)),
+		make([]string, 0, len(n.StrList)),
+	}
 
-const defaultTTL = 20
+	for i, node := range n.List {
+		if node != nil {
+			filtered.List = append(filtered.List, node)
+		}
+
+		if openNetwork {
+			// we want to store raw enodes for later checks
+			filtered.StrList = append(filtered.StrList, n.StrList[i])
+		}
+	}
+
+	return filtered
+}
+
+func getParseFunc(openNetwork bool) func(string) (*enode.Node, error) {
+	getEnode := enode.ParseV4WithResolve
+	if !openNetwork {
+		getEnode = enode.GetParseV4WithResolveMaxTry(maxParseTries, delayBetweenTries)
+	}
+	return getEnode
+}
+
+var cache = &domainCache{m: make(map[string]resolvedNode)}
 
 type domainCache struct {
 	m map[string]resolvedNode
@@ -49,7 +104,7 @@ type domainCache struct {
 }
 
 type resolvedNode struct {
-	node *enode.Node
+	node  *enode.Node
 	count int
 }
 
