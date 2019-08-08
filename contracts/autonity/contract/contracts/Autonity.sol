8df501ce9e1d6c4ba5fb8130e48d1d59a75e2697
b00ris <b00ris@mail.ru>
2019-08-08 13:02:26 +0300

    Stacking contract prototype (#223)
    * stake contract prototype
    
    * cleanup autonity contract
    
    * add minGasPrice logic
    
    * reformat

--- contracts/autonity/contract/contracts/Autonity.sol
+++ contracts/autonity/contract/contracts/Autonity.sol
@@ -1,7 +1,10 @@
 pragma solidity ^0.5.1;
 pragma experimental ABIEncoderV2;
+import "./SafeMath.sol";
+
 
 contract Autonity {
+    using SafeMath for uint256;
 
     // validators - list of validators of network
     address[] public validators;
@@ -11,6 +14,54 @@ contract Autonity {
     address public owner;
     // governanceOperatorAccount - account who can manipulate enodesWhitelist
     address public governanceOperatorAccount;
+
+    /*
+    * The bonding period (BP) is specified by the Autonity System Architecture as an integer representing an interval of blocks.
+    * We have identifed two differents ways to how this parameter could be used :
+    * 1. Bonding/unbonding operations happening at the end of each epoch.
+    * 2. BP-Delayed unbonding.
+    */
+    uint256 public bonding_period = 100;
+    /*
+    * The commission rate is set globally at the member level and is public:
+    * A member can’t have multiple commission rates depending on the member
+    * The commission rate MUST be by default 0 and MUST remain unchanged if not updated.
+    */
+    mapping (address => uint256) private comission_rate;
+
+    /*
+    * mapping of members who are able to use stacking
+    */
+    mapping (address => bool) private members;
+
+    /*
+    * unbonded stake token balance
+    */
+    mapping (address => uint256) private stake_token;
+    /*
+    * bonded stake token balance
+    */
+    mapping (address => uint256) private bonded_stake_token;
+    /*
+    * delegated stake token balance.
+    * map[owner address][delegator address] stake
+    */
+    mapping(address => mapping(address => uint)) delegated_stake_token;
+
+
+    struct unbondingStake {
+        uint256 amount;
+        uint256 block_number;
+    }
+    mapping (address => mapping(address => unbondingStake[])) private unbonding_stake_token;
+
+
+
+    /*
+    * Ethereum transactions gas price must be greater or equal to the minimumGasPrice, a value set by the Governance operator.
+    * FM-REQ-5: The minimumGasPrice value is a Genesis file configuration, if ommitted it defaults to 0.
+    */
+    uint256 minGasPrice = 0;
 
 
 
@@ -29,12 +80,20 @@ contract Autonity {
     }
 
 
+    /*
+    * AddValidator
+    * Add validator to validators list. Could be
+    */
     function AddValidator(address _validator) public onlyValidators(msg.sender) {
         //Need to make sure we're duplicating the entry
         validators.push(_validator);
     }
 
 
+    /*
+    * RemoveValidator
+    * Remove validator from validators list. function MUST be restricted to the Authority Account.
+    */
     function RemoveValidator(address _validator) public onlyValidators(msg.sender) {
         require(validators.length > 1);
 
@@ -48,12 +107,21 @@ contract Autonity {
 
     }
 
+    /*
+    * AddEnode
+    * add enode to permission list
+    * function MUST be restricted to the Authority Account.
+    */
     function AddEnode(string memory  _enode) public onlyGovernanceOperator(msg.sender) {
         //Need to make sure we're not duplicating the entry
         enodesWhitelist.push(_enode);
     }
 
-
+    /*
+    * RemoveEnode
+    * remove enode from permission list
+    * function MUST be restricted to the Authority Account.
+    */
     function RemoveEnode(string memory  _enode) public onlyGovernanceOperator(msg.sender) {
         require(enodesWhitelist.length > 1);
 
@@ -67,9 +135,109 @@ contract Autonity {
 
     }
 
-    function compareStringsbyBytes(string memory s1, string memory s2) public pure returns(bool){
-        return keccak256(abi.encodePacked(s1)) == keccak256(abi.encodePacked(s2));
+    /*
+    * setMinimumGasPrice
+    * FM-REQ-4: The Autonity Contract implements the setMinimumGasPrice function that is restricted to the Governance Operator account.
+    * The function takes as an argument a positive integer and modify the value of minimumGasPrice
+    */
+    function SetMinimumGasPrice(uint256  _value) public onlyGovernanceOperator(msg.sender) {
+        minGasPrice = _value;
     }
+
+
+    /*
+    * MintStake
+    * function capable of creating new stake token and adding it to the recipient balance
+    * function MUST be restricted to theAuthority Account.
+    */
+    function MintStake(address _account, uint256 _amount) public onlyGovernanceOperator(msg.sender) {
+        require(members[_account] == true, "Account hasn't created");
+        stake_token[_account] = stake_token[_account].add(_amount);
+    }
+
+    /*
+    * RedeemStake
+    * Decrease unbonded stake
+    * The redeemStake(amount, recipient) function MUST be restricted to the Authority Account.
+    */
+    function RedeemStake(address _account, uint256 _amount) public onlyGovernanceOperator(msg.sender) {
+        require(members[_account] == true, "Account hasn't created");
+        stake_token[_account] =  stake_token[_account].sub(_amount, "Redeem stake amount exceeds balance");
+    }
+
+
+    /*
+    * AddNewMember
+    * Add not nil account to members list
+    * function MUST be restricted to the Authority Account.
+    */
+    function AddNewMember(address _account) public onlyGovernanceOperator(msg.sender) {
+        require(_account != address(0), "Account is empty");
+        require(members[_account] == false, "Account has already created");
+        members[_account] = true;
+    }
+
+
+    /*
+    * RemoveMember
+    * Remove account from members list
+    * function MUST be restricted to the Authority Account.
+    */
+    function RemoveMember(address _account) public onlyGovernanceOperator(msg.sender) {
+        require(members[_account] == true, "Account hasn't created");
+        members[_account] = false;
+    }
+
+
+
+    /*
+    * send
+    * Moves `amount` stake tokens from the caller's account to `recipient`.
+    *
+    * Returns a boolean value indicating whether the operation succeeded.
+    *
+    * Emits a {Transfer} event.
+    */
+    function send(address _recipient, uint256 _amount) external returns (bool) {
+        require(members[msg.sender] == true, "Account hasn't created");
+        require(members[_recipient] == true, "Account hasn't created");
+        _transfer(msg.sender, _recipient, _amount);
+        return true;
+    }
+
+
+    // The Autonity Contract MUST implements the bondStake(amount, recipient) function capable of delegating stake token.
+    function Bonding(address _recipient, uint256 amount) public returns (bool){
+        require(members[msg.sender] == true, "Account hasn't created");
+        require(members[_recipient] == true, "Account hasn't created");
+
+        stake_token[msg.sender] = stake_token[msg.sender].sub(amount);
+        bonded_stake_token[_recipient] = bonded_stake_token[_recipient].add(amount);
+        delegated_stake_token[msg.sender][_recipient] = delegated_stake_token[msg.sender][_recipient].add(amount);
+    }
+
+
+
+
+    function Unbonding(address _recipient, uint256 _amount) public returns (bool){
+        require(members[msg.sender] == true, "Account hasn't created");
+        require(members[_recipient] == true, "Account hasn't created");
+
+        bonded_stake_token[_recipient] = bonded_stake_token[_recipient].sub(_amount);
+        delegated_stake_token[msg.sender][_recipient] = delegated_stake_token[msg.sender][_recipient].sub(_amount);
+        unbonding_stake_token[msg.sender][_recipient].push(unbondingStake(_amount,  block.number + bonding_period));
+    }
+
+
+    //    The Autonity Contract MUST implements the setCommissionRate(rate)
+    //    function capable of fixing the caller commission rate for the next bonding period.
+    function SetCommissionRate(uint256 rate) public returns(bool)  {
+        require(members[msg.sender] == true, "Account hasn't created");
+        comission_rate[msg.sender] = rate;
+        return true;
+    }
+
+
 
     /*
     ========================================================================================================================
@@ -95,8 +263,81 @@ contract Autonity {
     * Returns the macro participants list
     */
 
-    function getWhitelist() public view returns (string[] memory) {
+    function GetWhitelist() public view returns (string[] memory) {
         return enodesWhitelist;
+    }
+
+
+    /*
+    * GetAccountStake
+    *
+    * Returns unbonded stake for account
+    */
+    function GetAccountStake(address _account) public view returns (uint256) {
+        return stake_token[_account];
+    }
+
+
+    /*
+    * CheckMember
+    *
+    * Returns is addres a member
+    */
+    function CheckMember(address _account) public view returns (bool) {
+        return members[_account];
+    }
+
+    /*
+    * GetStake
+    *
+    * Returns sender's unbonded stake
+    */
+    function GetStake() public view returns(uint256) {
+        return stake_token[msg.sender];
+    }
+
+    /*
+    * GetBondedStake
+    *
+    * Returns sender's ubonded stake
+    */
+    function GetBondedStake() public view returns(uint256) {
+        return bonded_stake_token[msg.sender];
+    }
+
+    /*
+    * GetDelegatedBondedStake
+    *
+    * Returns sender's deleagated to _account
+    */
+    function GetDelegatedBondedStake(address _account) public view returns(uint256) {
+        return delegated_stake_token[msg.sender][_account];
+    }
+
+
+    function getRate(address _account) public view returns(uint256) {
+        require(members[msg.sender] == true, "Account hasn't created");
+        return comission_rate[msg.sender];
+    }
+
+    /*
+    * GetUnbondingStake
+    *
+    * Returns sender's unbonding stake by account
+    */
+    function GetUnbondingStake(address _account) public view returns(unbondingStake[] memory ) {
+        return unbonding_stake_token[msg.sender][_account];
+    }
+
+
+
+
+    /*
+    * GetMinimumGasPrice
+    * Returns minimum gas price. Ethereum transactions gas price must be greater or equal to the minimumGasPrice.
+    */
+    function GetMinimumGasPrice() public view returns(uint256) {
+        return minGasPrice;
     }
 
 
@@ -135,4 +376,42 @@ contract Autonity {
         require(governanceOperatorAccount == _caller, "Caller is not a operator");
         _;
     }
+
+
+
+    /*
+    ========================================================================================================================
+
+        Events
+
+    ========================================================================================================================
+    */
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+
+    /*
+    ========================================================================================================================
+
+        Internal
+
+    ========================================================================================================================
+    */
+
+
+
+    function _transfer(address sender, address recipient, uint256 amount) internal {
+        require(sender != address(0), "Transfer from the zero address");
+        require(recipient != address(0), "Transfer to the zero address");
+
+        stake_token[sender] = stake_token[sender].sub(amount, "Transfer amount exceeds balance");
+        stake_token[recipient] = stake_token[recipient].add(amount);
+        emit Transfer(sender, recipient, amount);
+    }
+
+
+    function compareStringsbyBytes(string memory s1, string memory s2) internal pure returns(bool){
+        return keccak256(abi.encodePacked(s1)) == keccak256(abi.encodePacked(s2));
+    }
+
 }
--- contracts/autonity/contract/test/test.js
+++ contracts/autonity/contract/test/test.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//it's kind of e2e test. They have sequence dependency
+//todo get rid of it.
 
 const Autonity = artifacts.require('Autonity.sol');
 const validatorsList = [
@@ -18,14 +20,17 @@ const whiteList = [
     "enode://3ce6c053cb563bfd94f4e0e248510a07ccee1bc836c9784da1816dba4b10564e7be1ba42e0bd8d73c8f6274f8e9878dc13814adb381c823264265c06048b4b59@172.25.0.15:30303"
 ];
 
+const governanceOperatorAccount = "0x627306090abaB3A6e1400e9345bC60c78a8BEf57";
+
 contract('Autonity', function(accounts) {
 
     it('test validator can get validator list', async function() {
         const token = await Autonity.deployed();
 
-        var getValidatorsResult = await token.GetValidators({from:accounts[0]});
+        var getValidatorsResult = await token.GetValidators({from:governanceOperatorAccount});
         assert.deepEqual(getValidatorsResult, validatorsList);
     });
+
 
     it('test non validator can get validator list', async function() {
         const token = await Autonity.deployed();
@@ -42,30 +47,30 @@ contract('Autonity', function(accounts) {
             var r =await token.AddValidator(accounts[7], {from:accounts[6]})
 
         } catch (e) {
-            //skip error
+            var getValidatorsResult = await token.GetValidators({from:governanceOperatorAccount});
+            assert.deepEqual(getValidatorsResult, validatorsList);
+            return
         }
 
-        var getValidatorsResult = await token.GetValidators({from:accounts[0]});
-        assert.deepEqual(getValidatorsResult, validatorsList)
+        assert.fail('Expected throw not received');
     });
+
 
     it('test add validator and check that it is in get validator list', async function() {
         const token = await Autonity.deployed();
 
-        token.AddValidator(accounts[7], {from:accounts[0]})
+        token.AddValidator(accounts[7], {from:governanceOperatorAccount});
 
-        var getValidatorsResult = await token.GetValidators({from:accounts[0]});
+        var getValidatorsResult = await token.GetValidators({from:governanceOperatorAccount});
 
         var expected = validatorsList;
         expected.push(accounts[7]);
-        assert.deepEqual(getValidatorsResult, expected)
+        assert.deepEqual(getValidatorsResult, expected);
 
-        token.RemoveValidator(accounts[7], {from:accounts[0]})
-        getValidatorsResult = await token.GetValidators({from:accounts[0]});
+        token.RemoveValidator(accounts[7], {from:governanceOperatorAccount});
+        getValidatorsResult = await token.GetValidators({from:governanceOperatorAccount});
         assert.deepEqual(getValidatorsResult, validatorsList)
     });
-
-
 
 
     it('test non Governance operator cant add to whitelist', async function() {
@@ -75,12 +80,15 @@ contract('Autonity', function(accounts) {
             var r =await token.RemoveEnode(enode, {from:accounts[6]})
 
         } catch (e) {
-            //skip error
+            var getWhitelistResult = await token.GetWhitelist({from:governanceOperatorAccount});
+            assert.deepEqual(getWhitelistResult, whiteList);
+            return
         }
 
-        var getWhitelistResult = await token.getWhitelist({from:accounts[0]});
-        assert.deepEqual(getWhitelistResult, whiteList)
+        assert.fail('Expected throw not received');
+
     });
+
 
     it('test non Governance operator cant remove from whitelist', async function() {
         const token = await Autonity.deployed();
@@ -90,25 +98,207 @@ contract('Autonity', function(accounts) {
 
         } catch (e) {
             //skip error
+            var getWhitelistResult = await token.GetWhitelist({from:governanceOperatorAccount});
+            assert.deepEqual(getWhitelistResult, whiteList);
+            return
         }
 
-        var getWhitelistResult = await token.getWhitelist({from:accounts[0]});
-        assert.deepEqual(getWhitelistResult, whiteList)
+        assert.fail('Expected throw not received');
     });
 
 
     it('test Governance operator can add/remove to whitelist', async function() {
         const token = await Autonity.deployed();
         var enode = "enode://testenode";
-        await token.AddEnode(enode, {from:accounts[0]});
+        await token.AddEnode(enode, {from:governanceOperatorAccount});
 
-        var getValidatorsResult = await token.getWhitelist({from:accounts[0]});
+        var getValidatorsResult = await token.GetWhitelist({from:governanceOperatorAccount});
         var expected = whiteList;
         expected.push(enode);
         assert.deepEqual(getValidatorsResult, expected);
 
-        await token.RemoveEnode(enode,{from:accounts[0]});
-        var getValidatorsResult = await token.getWhitelist({from:accounts[1]});
+        await token.RemoveEnode(enode,{from:governanceOperatorAccount});
+        getValidatorsResult = await token.GetWhitelist({from:accounts[1]});
         assert.deepEqual(getValidatorsResult, whiteList);
     });
+
+
+    it('test create account check it and remove it', async function() {
+        const token = await Autonity.deployed();
+
+        await token.AddNewMember(accounts[5], {from: governanceOperatorAccount});
+        var addMemberResult=await token.CheckMember(accounts[5]);
+
+        assert(true == addMemberResult);
+
+        await token.RemoveMember(accounts[5], {from: governanceOperatorAccount});
+        var removeMemberResult=await token.CheckMember(accounts[5]);
+
+        assert(false == removeMemberResult);
+    });
+
+
+    it('test create account, add stake, check that it is added, remove stake', async function() {
+        const token = await Autonity.deployed();
+
+        await token.AddNewMember(accounts[7], {from:governanceOperatorAccount});
+        var getStakeResult = await token.GetStake(accounts[7], {from:accounts[7]});
+        assert(0 == getStakeResult, "unexpected tokens");
+
+        await token.MintStake(accounts[7], 100, {from:governanceOperatorAccount});
+
+        getStakeResult = await token.GetStake(accounts[7], {from:accounts[7]});
+        assert(100 == getStakeResult, "tokens are not minted");
+
+        await token.RedeemStake(accounts[7], 100, {from:governanceOperatorAccount});
+
+        getStakeResult = await token.GetStake(accounts[7], {from:accounts[7]});
+        assert(0 == getStakeResult, "unexpected tokens");
+
+        await token.RemoveMember(accounts[7], {from: governanceOperatorAccount});
+    });
+
+
+    it('test create account, get error when redeem empty stake', async function() {
+        const token = await Autonity.deployed();
+
+        await token.AddNewMember(accounts[5], {from:governanceOperatorAccount});
+        var getStakeResult = await token.GetStake(accounts[5], {from:accounts[5]});
+        assert(0 == getStakeResult, "unexpected tokens not minted");
+
+        try {
+            await token.RedeemStake(accounts[5], 100, {from:governanceOperatorAccount});
+        } catch (e) {
+            getStakeResult = await token.GetStake(accounts[5], {from:accounts[7]});
+            assert(0 == getStakeResult, "unexpected tokens");
+            await token.RemoveMember(accounts[5], {from: governanceOperatorAccount});
+            return
+        }
+
+        assert.fail('Expected throw not received');
+    });
+
+    it('test create/remove account by non governance operator', async function() {
+        const token = await Autonity.deployed();
+        var errorOnAddNewMember = false;
+        var errorOnRemoveMember = false;
+
+
+        try {
+            await token.AddNewMember(accounts[4], {from: accounts[7]});
+        }catch (e) {
+            errorOnAddNewMember = true
+        }
+        var addMemberResult=await token.CheckMember(accounts[4]);
+        assert(false == addMemberResult);
+
+        await token.AddNewMember(accounts[4], {from: governanceOperatorAccount});
+
+        addMemberResult=await token.CheckMember(accounts[4]);
+        assert(true == addMemberResult);
+
+
+        try {
+            await token.RemoveMember(accounts[4], {from:  accounts[7]});
+        }catch (e) {
+            errorOnRemoveMember = true
+        }
+
+        var removeMemberResult=await token.CheckMember(accounts[4]);
+        assert(true == removeMemberResult);
+        await token.RemoveMember(accounts[4], {from: governanceOperatorAccount});
+
+        removeMemberResult=await token.CheckMember(accounts[4]);
+        assert(false == removeMemberResult);
+
+        assert(true == errorOnAddNewMember);
+        assert(true == errorOnRemoveMember);
+
+    });
+
+    it('test transfer stake', async function() {
+        const token = await Autonity.deployed();
+
+        await token.AddNewMember(accounts[7], {from:governanceOperatorAccount});
+        var getStakeResult = await token.GetStake(accounts[7], {from:accounts[7]});
+        assert(0 == getStakeResult, "unexpected tokens");
+
+        await token.AddNewMember(accounts[5], {from:governanceOperatorAccount});
+        var getStakeResult = await token.GetStake(accounts[5], {from:accounts[5]});
+        assert(0 == getStakeResult, "unexpected tokens");
+
+        await token.MintStake(accounts[7], 100, {from:governanceOperatorAccount});
+
+        getStakeResult = await token.GetStake(accounts[7], {from:accounts[7]});
+        assert(100 == getStakeResult, "tokens are not minted");
+
+        await token.send(accounts[5], 50, {from:accounts[7]});
+
+        getStakeResult = await token.GetStake(accounts[7], {from:accounts[7]});
+        assert(50 == getStakeResult, "unexpected tokens");
+
+        getStakeResult = await token.GetStake(accounts[5], {from:accounts[5]});
+        assert(50 == getStakeResult, "unexpected tokens");
+
+        await token.RedeemStake(accounts[7],50, {from:governanceOperatorAccount});
+        await token.RedeemStake(accounts[5],50, {from:governanceOperatorAccount});
+        await token.RemoveMember(accounts[7], {from:governanceOperatorAccount});
+        await token.RemoveMember(accounts[5], {from:governanceOperatorAccount});
+    });
+
+
+    it('test bond stake', async function() {
+        const token = await Autonity.deployed();
+
+        await token.AddNewMember(accounts[7], {from:governanceOperatorAccount});
+        var getStakeResult = await token.GetStake(accounts[7], {from:accounts[7]});
+        assert(0 == getStakeResult, "unexpected tokens");
+
+        await token.MintStake(accounts[7], 100, {from:governanceOperatorAccount});
+
+        getStakeResult = await token.GetStake(accounts[7], {from:accounts[7]});
+        assert(100 == getStakeResult, "tokens are not minted");
+
+        await token.Bonding(accounts[7], 30, {from:accounts[7]});
+
+        getStakeResult = await token.GetStake(accounts[7], {from:accounts[7]});
+        assert(70 == getStakeResult, "stake is incorrect");
+
+        getStakeResult = await token.GetBondedStake(accounts[7], {from:accounts[7]});
+        assert(30 == getStakeResult, "bonded stake is incorrect");
+
+        getStakeResult = await token.GetDelegatedBondedStake(accounts[7], {from:accounts[7]});
+        assert(30 == getStakeResult, "getDelegatedBondedStake is incorrect");
+
+    });
+
+    it('test unbond stake', async function() {
+        const token = await Autonity.deployed();
+
+        var getStakeResult = await token.GetStake(accounts[7], {from:accounts[7]});
+        assert(70 == getStakeResult, "unexpected tokens");
+
+        getStakeResult = await token.GetBondedStake(accounts[7], {from:accounts[7]});
+        assert(30 == getStakeResult, "bonded stake is incorrect");
+
+        getStakeResult = await token.GetDelegatedBondedStake(accounts[7], {from:accounts[7]});
+        assert(30 == getStakeResult, "getDelegatedBondedStake is incorrect");
+
+
+        await token.Unbonding(accounts[7], 15, {from:accounts[7]});
+
+        getStakeResult = await token.GetStake(accounts[7], {from:accounts[7]});
+        assert(70 == getStakeResult, "stake is incorrect");
+
+        getStakeResult = await token.GetBondedStake(accounts[7], {from:accounts[7]});
+        assert(15 == getStakeResult, "bonded stake is incorrect");
+
+        getStakeResult = await token.GetDelegatedBondedStake(accounts[7], {from:accounts[7]});
+        assert(15 == getStakeResult, "getDelegatedBondedStake is incorrect");
+
+
+        await token.GetUnbondingStake(accounts[7], {from:accounts[7]});
+        await token.RemoveMember(accounts[7], {from:governanceOperatorAccount});
+    });
+
 });
