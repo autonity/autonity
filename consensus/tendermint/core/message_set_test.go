95cff1669d6c13108eda05071468c901c89eb19e
Max O'Donnell <screwyprof@gmail.com>
2019-09-09 12:53:40 +0300

    Add tendermint core tests (part 1) (#261)
    * Add core types tests
    
    * Add core message set tests
    
    * Add core message tests
    
    * Add core backlog tests

---
+++ consensus/tendermint/core/message_set_test.go
@@ -0,0 +1,80 @@
+package core
+
+import (
+	"testing"
+
+	"github.com/clearmatics/autonity/common"
+)
+
+func TestMessageSetAddVote(t *testing.T) {
+	blockHash := common.BytesToHash([]byte("123456789"))
+	msg := message{Address: common.BytesToAddress([]byte("987654321"))}
+
+	ms := newMessageSet()
+	ms.AddVote(blockHash, msg)
+	ms.AddVote(blockHash, msg)
+
+	if got := ms.VotesSize(blockHash); got != 1 {
+		t.Fatalf("Expected 1 vote, got %v", got)
+	}
+}
+
+func TestMessageSetVotesSize(t *testing.T) {
+	blockHash := common.BytesToHash([]byte("123456789"))
+
+	ms := newMessageSet()
+	if got := ms.VotesSize(blockHash); got != 0 {
+		t.Fatalf("Expected 0, got %v", got)
+	}
+}
+
+func TestMessageSetAddNilVote(t *testing.T) {
+	msg := message{Address: common.BytesToAddress([]byte("987654321"))}
+
+	ms := newMessageSet()
+	ms.AddNilVote(msg)
+	ms.AddNilVote(msg)
+
+	if got := ms.NilVotesSize(); got != 1 {
+		t.Fatalf("Expected 1 nil vote, got %v", got)
+	}
+}
+
+func TestMessageSetTotalSize(t *testing.T) {
+	blockHash := common.BytesToHash([]byte("123456789"))
+	msg := message{Address: common.BytesToAddress([]byte("987654321"))}
+
+	ms := newMessageSet()
+	ms.AddVote(blockHash, msg)
+	ms.AddVote(blockHash, msg)
+
+	ms.AddNilVote(msg)
+	ms.AddNilVote(msg)
+
+	if got := ms.TotalSize(); got != 2 {
+		t.Fatalf("Expected 2 total votes, got %v", got)
+	}
+}
+
+func TestMessageSetValues(t *testing.T) {
+	t.Run("not known hash given, nil returned", func(t *testing.T) {
+		blockHash := common.BytesToHash([]byte("123456789"))
+		ms := newMessageSet()
+
+		if got := ms.Values(blockHash); got != nil {
+			t.Fatalf("Expected nils, got %v", got)
+		}
+	})
+
+	t.Run("known hash given, message returned", func(t *testing.T) {
+		blockHash := common.BytesToHash([]byte("123456789"))
+		msg := message{Address: common.BytesToAddress([]byte("987654321"))}
+
+		ms := newMessageSet()
+		ms.AddVote(blockHash, msg)
+
+		if got := len(ms.Values(blockHash)); got != 1 {
+			t.Fatalf("Expected 1 message, got %v", got)
+		}
+	})
+}
