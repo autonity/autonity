5257d3f3e649f12da6325865bab529155ea96b65
Aneeque Safdar <aneequesafdar@gmail.com>
2019-09-11 12:41:55 +0100

    increase blocksToWait before checking the transactions in blocks

--- consensus/test/tendermint_test.go
+++ consensus/test/tendermint_test.go
@@ -722,7 +722,7 @@ func (validator *testNode) startService() error {
 }
 
 func sendTransactions(t *testing.T, test *testCase, validators []*testNode, txPerPeer int, errorOnTx bool) {
-	const blocksToWait = 10
+	const blocksToWait = 40
 
 	txs := make(map[uint64]int) // blockNumber to count
 	txsMu := sync.Mutex{}
