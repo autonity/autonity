b20220a1015c35ad078aab9ad6ace0e90d90bcaa
Max O'Donnell <screwyprof@gmail.com>
2019-09-21 21:38:59 +0300

    Add tendermint core precommit tests

---
+++ consensus/tendermint/core/precommit_test.go
@@ -0,0 +1,265 @@
+package core
+
+import (
+	"context"
+	"math/big"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+
+	"github.com/clearmatics/autonity/common"
+	"github.com/clearmatics/autonity/core/types"
+	"github.com/clearmatics/autonity/crypto"
+	"github.com/clearmatics/autonity/crypto/secp256k1"
+	"github.com/clearmatics/autonity/log"
+)
+
+func TestHandlePrecommit(t *testing.T) {
+	t.Run("pre-commit with future height given, error returned", func(t *testing.T) {
+		curRoundState := NewRoundState(big.NewInt(1), big.NewInt(2))
+		addr := common.HexToAddress("0x0123456789")
+
+		var preCommit = Vote{
+			Round:  big.NewInt(2),
+			Height: big.NewInt(3),
+		}
+
+		encodedVote, err := Encode(&preCommit)
+		if err != nil {
+			t.Fatalf("Expected nil, got %v", err)
+		}
+
+		expectedMsg := &Message{
+			Code:          msgPrecommit,
+			Msg:           encodedVote,
+			Address:       addr,
+			CommittedSeal: []byte{},
+			Signature:     []byte{0x1},
+		}
+
+		c := &core{
+			address:           addr,
+			currentRoundState: curRoundState,
+			logger:            log.New("backend", "test", "id", 0),
+			valSet:            new(validatorSet),
+		}
+
+		err = c.handlePrecommit(context.Background(), expectedMsg)
+		if err != errFutureHeightMessage {
+			t.Fatalf("Expected %v, got %v", errFutureHeightMessage, err)
+		}
+	})
+
+	t.Run("pre-commit with invalid signature given, error returned", func(t *testing.T) {
+		curRoundState := NewRoundState(big.NewInt(2), big.NewInt(3))
+		addr := common.HexToAddress("0x0123456789")
+
+		var preCommit = Vote{
+			Round:             big.NewInt(curRoundState.Round().Int64()),
+			Height:            big.NewInt(curRoundState.Height().Int64()),
+			ProposedBlockHash: curRoundState.GetCurrentProposalHash(),
+		}
+
+		encodedVote, err := Encode(&preCommit)
+		if err != nil {
+			t.Fatalf("Expected nil, got %v", err)
+		}
+
+		expectedMsg := &Message{
+			Code:          msgPrecommit,
+			Msg:           encodedVote,
+			Address:       addr,
+			CommittedSeal: []byte{},
+			Signature:     []byte{0x1},
+		}
+
+		c := &core{
+			address:           addr,
+			currentRoundState: curRoundState,
+			logger:            log.New("backend", "test", "id", 0),
+			valSet:            new(validatorSet),
+		}
+
+		err = c.handlePrecommit(context.Background(), expectedMsg)
+		if err != secp256k1.ErrInvalidSignatureLen {
+			t.Fatalf("Expected %v, got %v", secp256k1.ErrInvalidSignatureLen, err)
+		}
+	})
+
+	t.Run("pre-commit given with no errors, commit called", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		proposal := NewProposal(
+			big.NewInt(1),
+			big.NewInt(2),
+			big.NewInt(1),
+			types.NewBlockWithHeader(&types.Header{}))
+
+		curRoundState := NewRoundState(big.NewInt(2), big.NewInt(3))
+		curRoundState.SetProposal(proposal, nil)
+
+		addr := getAddress()
+
+		var preCommit = Vote{
+			Round:             big.NewInt(curRoundState.Round().Int64()),
+			Height:            big.NewInt(curRoundState.Height().Int64()),
+			ProposedBlockHash: addr.Hash(),
+		}
+
+		encodedVote, err := Encode(&preCommit)
+		if err != nil {
+			t.Fatalf("Expected nil, got %v", err)
+		}
+
+		key, err := generatePrivateKey()
+		if err != nil {
+			t.Fatalf("Expected nil, got %v", err)
+		}
+
+		data := PrepareCommittedSeal(addr.Hash())
+		hashData := crypto.Keccak256(data)
+		sig, err := crypto.Sign(hashData, key)
+		if err != nil {
+			t.Fatalf("Expected nil, got %v", err)
+		}
+
+		expectedMsg := &Message{
+			Code:          msgPrecommit,
+			Msg:           encodedVote,
+			Address:       addr,
+			CommittedSeal: sig,
+			Signature:     []byte{0x1},
+		}
+
+		backendMock := NewMockBackend(ctrl)
+		//backendMock.EXPECT().Commit(*proposal.ProposalBlock, gomock.Any()).Return(nil)
+		backendMock.EXPECT().Commit(gomock.Any(), gomock.Any()).Return(nil)
+
+		c := &core{
+			address:           addr,
+			backend:           backendMock,
+			currentRoundState: curRoundState,
+			logger:            log.New("backend", "test", "id", 0),
+			valSet:            new(validatorSet),
+			precommitTimeout:  newTimeout(precommit),
+		}
+
+		err = c.handlePrecommit(context.Background(), expectedMsg)
+		if err != nil {
+			t.Fatalf("Expected nil, got %v", err)
+		}
+	})
+
+	t.Run("pre-commit given with no errors, pre-commit timeout triggered", func(t *testing.T) {
+		curRoundState := NewRoundState(big.NewInt(2), big.NewInt(3))
+		addr := getAddress()
+
+		var preCommit = Vote{
+			Round:             big.NewInt(curRoundState.Round().Int64()),
+			Height:            big.NewInt(curRoundState.Height().Int64()),
+			ProposedBlockHash: addr.Hash(),
+		}
+
+		encodedVote, err := Encode(&preCommit)
+		if err != nil {
+			t.Fatalf("Expected nil, got %v", err)
+		}
+
+		key, err := generatePrivateKey()
+		if err != nil {
+			t.Fatalf("Expected nil, got %v", err)
+		}
+
+		data := PrepareCommittedSeal(addr.Hash())
+		hashData := crypto.Keccak256(data)
+		sig, err := crypto.Sign(hashData, key)
+		if err != nil {
+			t.Fatalf("Expected nil, got %v", err)
+		}
+
+		expectedMsg := &Message{
+			Code:          msgPrecommit,
+			Msg:           encodedVote,
+			Address:       addr,
+			CommittedSeal: sig,
+			Signature:     []byte{0x1},
+		}
+
+		c := &core{
+			address:           addr,
+			currentRoundState: curRoundState,
+			logger:            log.New("backend", "test", "id", 0),
+			valSet:            new(validatorSet),
+			precommitTimeout:  newTimeout(precommit),
+		}
+
+		err = c.handlePrecommit(context.Background(), expectedMsg)
+		if err != nil {
+			t.Fatalf("Expected nil, got %v", err)
+		}
+	})
+}
+
+func TestVerifyPrecommitCommittedSeal(t *testing.T) {
+	t.Run("invalid signer address given, error returned", func(t *testing.T) {
+		c := &core{
+			logger: log.New("backend", "test", "id", 0),
+		}
+
+		addrMsg := common.HexToAddress("0x0123456789")
+
+		err := c.verifyPrecommitCommittedSeal(addrMsg, nil, common.Hash{})
+		if err != secp256k1.ErrInvalidSignatureLen {
+			t.Fatalf("Expected %v, got %v", secp256k1.ErrInvalidSignatureLen, err)
+		}
+	})
+
+	t.Run("invalid signer address given, error returned", func(t *testing.T) {
+		c := &core{
+			logger: log.New("backend", "test", "id", 0),
+		}
+
+		addrMsg := common.HexToAddress("0x0123456789")
+		key, err := generatePrivateKey()
+		if err != nil {
+			t.Fatalf("Expected nil, got %v", err)
+		}
+
+		data := PrepareCommittedSeal(common.Hash{})
+		hashData := crypto.Keccak256(data)
+		sig, err := crypto.Sign(hashData, key)
+		if err != nil {
+			t.Fatalf("Expected nil, got %v", err)
+		}
+
+		err = c.verifyPrecommitCommittedSeal(addrMsg, sig, common.Hash{})
+		if err != errInvalidSenderOfCommittedSeal {
+			t.Fatalf("Expected %v, got %v", errInvalidSenderOfCommittedSeal, err)
+		}
+	})
+
+	t.Run("valid params given, no error returned", func(t *testing.T) {
+		c := &core{
+			logger: log.New("backend", "test", "id", 0),
+		}
+
+		addrMsg := getAddress()
+		key, err := generatePrivateKey()
+		if err != nil {
+			t.Fatalf("Expected nil, got %v", err)
+		}
+
+		data := PrepareCommittedSeal(addrMsg.Hash())
+		hashData := crypto.Keccak256(data)
+		sig, err := crypto.Sign(hashData, key)
+		if err != nil {
+			t.Fatalf("Expected nil, got %v", err)
+		}
+
+		err = c.verifyPrecommitCommittedSeal(addrMsg, sig, addrMsg.Hash())
+		if err != nil {
+			t.Fatalf("Expected nil, got %v", err)
+		}
+	})
+}
