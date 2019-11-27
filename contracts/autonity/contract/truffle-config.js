591f15637f6b55d30f404f8f60750dc5cefdc51c
Jason Chen <jason.chen@clearmatics.com>
2019-11-27 15:43:47 +0000

    Integrate the ganache-cli which used by unit testframe truffle for contract testing.

--- contracts/autonity/contract/truffle-config.js
+++ contracts/autonity/contract/truffle-config.js
@@ -42,13 +42,13 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    //development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    //  gas: 46123880,
-    //  gasLimit: 2100000000000
-    // },
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+      gas: 46123880,
+      gasLimit: 2100000000000
+    },
     // test: {
     //    network_id: "*",       // Any network (default: none)
     //     gas: 46000000000
