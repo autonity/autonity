3e7c16b26e32be367e7e51fbc2ccbb7dd88cbf7e
Max O'Donnell <screwyprof@gmail.com>
2020-01-03 00:38:59 +0300

    Fix les

--- les/api_test.go
+++ les/api_test.go
@@ -504,7 +504,7 @@ func newLesServerService(ctx *adapters.ServiceContext) (node.Service, error) {
 	config.SyncMode = downloader.FullSync
 	config.LightServ = testServerCapacity
 	config.LightPeers = testMaxClients
-	ethereum, err := eth.New(ctx.NodeContext, &config)
+	ethereum, err := eth.New(ctx.NodeContext, &config, nil, nil)
 	if err != nil {
 		return nil, err
 	}
