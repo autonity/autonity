e6c9dcbea515c2dd074cf7e1f2e1cb39d97f2aa4
Evgeny Danienko <6655321@bk.ru>
2020-02-17 11:39:36 +0300

    fix tendermint core close test

--- consensus/tendermint/core/core_backend_test.go
+++ consensus/tendermint/core/core_backend_test.go
@@ -417,11 +417,13 @@ func TestCore_Close(t *testing.T) {
 
 		logger := log.New("backend", "test", "id", 0)
 
+		isStarted := uint32(1)
+
 		c := &core{
 			backend:                 backendMock,
 			cancel:                  cancel,
 			isStarting:              new(uint32),
-			isStarted:               new(uint32),
+			isStarted:               &isStarted,
 			isStopping:              new(uint32),
 			isStopped:               new(uint32),
 			committedSub:            committedSub,
