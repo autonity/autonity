0547d92ec3789da7010af799135119b6c364ec0a
b00ris <b00ris@mail.ru>
2020-02-26 13:39:49 +0300

    ignore leak

--- consensus/test/testcase_test.go
+++ consensus/test/testcase_test.go
@@ -2,7 +2,6 @@ package test
 
 import (
 	"fmt"
-	"github.com/clearmatics/autonity/common/keygenerator"
 	"net"
 	"os"
 	"strconv"
@@ -10,6 +9,8 @@ import (
 	"sync"
 	"testing"
 	"time"
+
+	"github.com/clearmatics/autonity/common/keygenerator"
 
 	"github.com/clearmatics/autonity/common"
 	"github.com/clearmatics/autonity/common/fdlimit"
@@ -125,6 +126,7 @@ func runTest(t *testing.T, test *testCase) {
 			goleak.IgnoreTopFunction("github.com/JekaMas/notify._Cfunc_CFRunLoopRun"),
 			goleak.IgnoreTopFunction("internal/poll.runtime_pollWait"),
 			goleak.IgnoreTopFunction("net/http.(*persistConn).writeLoop"),
+			goleak.IgnoreTopFunction("github.com/clearmatics/autonity/miner.(*worker).newWorkLoop.func1"),
 		)
 	}
 
