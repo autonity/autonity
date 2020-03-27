c79d0135efa8599d651ef4796878437e774f916b
Max O'Donnell <screwyprof@gmail.com>
2020-03-27 15:51:57 +0000

    Fix resolver (#484)
    * Fix resolver
    
    * Fix fmt
    
    * Fix linters
    
    * Add caching for invalid enodes
    
    * Fix go version
    
    * Fix test
    
    * use go 1.13
    
    * make tests run on 1.13 and 1.14
    
    * make tests run on 1.13 and 1.14
    
    * checkout if cache if not available
    
    * make tests run on 1.13 and 1.14
    
    Co-authored-by: Evgeny Danienko <6655321@bk.ru>

--- p2p/enode/urlv4_test.go
+++ p2p/enode/urlv4_test.go
@@ -158,12 +158,19 @@ func hexPubkey(h string) *ecdsa.PublicKey {
 func TestParseNode(t *testing.T) {
 	for _, test := range parseNodeTests {
 		n, err := Parse(ValidSchemes, test.input)
-		if test.wantError != "" {
+		var gotErr string
+		if err != nil {
+			gotErr = strings.ReplaceAll(err.Error(), "\"", "")
+		}
+
+		wantError := strings.ReplaceAll(test.wantError, "\"", "")
+
+		if wantError != "" {
 			if err == nil {
-				t.Errorf("test %q:\n  got nil error, expected %#q", test.input, test.wantError)
+				t.Errorf("test %q:\n  got nil error, expected %#q", test.input, wantError)
 				continue
-			} else if !strings.Contains(err.Error(), test.wantError) {
-				t.Errorf("test %q:\n  got error %#q, expected %#q\n%v", test.input, err.Error(), test.wantError, n)
+			} else if !strings.Contains(gotErr, wantError) {
+				t.Errorf("test %q:\n  got error %#q, expected %#q\n%v", test.input, gotErr, wantError, n)
 				continue
 			}
 		} else {
