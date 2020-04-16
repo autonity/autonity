b86795668e7644d0d40ee947c8e1146a6f10ed86
b00ris <b00ris@mail.ru>
2020-04-16 12:59:33 +0300

    tests

--- consensus/tendermint/committee/weighted_round_robin_test.go
+++ consensus/tendermint/committee/weighted_round_robin_test.go
@@ -1,6 +1,7 @@
 package committee
 
 import (
+	"fmt"
 	"github.com/clearmatics/autonity/core/types"
 	"math"
 	"math/big"
@@ -25,6 +26,134 @@ func TestWeightedRoundRobinProposer(t *testing.T) {
 	testWRRDetermination(t)
 	testNotScheduleZeroStakeHolder(t)
 	testWRRSchedule(t)
+}
+
+func TestWeightedRoundRobinProposerOperatorAttackVector(t *testing.T) {
+	b1 := common.Hex2Bytes(validator1)
+	b2 := common.Hex2Bytes(validator2)
+	b3 := common.Hex2Bytes(validator3)
+	b4 := common.Hex2Bytes(validator4)
+	b5 := common.Hex2Bytes(validator5)
+	b6 := common.Hex2Bytes(validator6)
+	addr1 := common.BytesToAddress(b1)
+	addr2 := common.BytesToAddress(b2)
+	addr3 := common.BytesToAddress(b3)
+	addr4 := common.BytesToAddress(b4)
+	addr5 := common.BytesToAddress(b5)
+	addr6 := common.BytesToAddress(b6)
+	val1 := types.CommitteeMember{Address: addr1, VotingPower: new(big.Int).SetUint64(100)}
+	val2 := types.CommitteeMember{Address: addr2, VotingPower: new(big.Int).SetUint64(100)}
+	val3 := types.CommitteeMember{Address: addr3, VotingPower: new(big.Int).SetUint64(100)}
+	val4 := types.CommitteeMember{Address: addr4, VotingPower: new(big.Int).SetUint64(100)}
+	val5 := types.CommitteeMember{Address: addr5, VotingPower: new(big.Int).SetUint64(100)}
+	val6 := types.CommitteeMember{Address: addr6, VotingPower: new(big.Int).SetUint64(100)}
+
+	//val1 - malicious
+
+	vals := types.Committee{val1, val2, val3, val4, val5, val6}
+	committeeSet, err := NewSet(types.Committee{val1, val2, val3, val4}, config.WeightedRoundRobin, val2.Address)
+	if committeeSet == nil || err != nil {
+		t.Errorf("the format of validator set is invalid")
+		t.FailNow()
+	}
+	h := int64(1)
+	r := int64(1)
+	nextCommitee := vals[:4]
+
+	nextLastProposer := addr2
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+		committeeSet, _ = NewSet(nextCommitee, config.WeightedRoundRobin, nextLastProposer)
+		c := committeeSet.GetProposer(r, big.NewInt(h))
+		nextLastProposer = c.Address
+		if c.Address == addr1 {
+			fmt.Println("mal")
+		} else {
+			//fmt.Println("not")
+		}
+		for j := 5; j > 3; j-- {
+			committeeSet, _ = NewSet(vals[:j], config.WeightedRoundRobin, c.Address)
+			c = committeeSet.GetProposer(r, big.NewInt(h))
+			if c.Address == addr1 {
+				nextCommitee = vals[:j]
+				//fmt.Println("c mal")
+				break
+			} else {
+				//fmt.Println("c not")
+			}
+		}
+		h++
+	}
+}
+
+func TestWeightedRoundRobinProposerValidatorsAttackVector(t *testing.T) {
+	b1 := common.Hex2Bytes(validator1)
+	b2 := common.Hex2Bytes(validator2)
+	b3 := common.Hex2Bytes(validator3)
+	b4 := common.Hex2Bytes(validator4)
+	addr1 := common.BytesToAddress(b1)
+	addr2 := common.BytesToAddress(b2)
+	addr3 := common.BytesToAddress(b3)
+	addr4 := common.BytesToAddress(b4)
+	val1 := types.CommitteeMember{Address: addr1, VotingPower: new(big.Int).SetUint64(100)}
+	val2 := types.CommitteeMember{Address: addr2, VotingPower: new(big.Int).SetUint64(100)}
+	val3 := types.CommitteeMember{Address: addr3, VotingPower: new(big.Int).SetUint64(100)}
+	val4 := types.CommitteeMember{Address: addr4, VotingPower: new(big.Int).SetUint64(200)}
+	committeeSet, err := NewSet(types.Committee{val1, val2, val3, val4}, config.WeightedRoundRobin, val1.Address)
+	if committeeSet == nil || err != nil {
+		t.Errorf("the format of validator set is invalid")
+		t.FailNow()
+	}
+	// v1, v2  is maligous
+	c := committeeSet.GetProposer(1, big.NewInt(1))
+	t.Log("round ", 1, "height", 1, c, "maligous validator block")
+	//ok
+	c2 := committeeSet.GetProposer(1, big.NewInt(2))
+	t.Log("round ", 1, "height", 2, c2, "maligous validator block")
+	//ok
+	c3 := committeeSet.GetProposer(1, big.NewInt(3))
+	t.Log("round ", 1, "height", 3, c3, "vote agains it")
+	//attack v1&v2 against block
+
+	c4 := committeeSet.GetProposer(2, big.NewInt(3))
+	t.Log("round ", 2, "height", 3, c4, "maligous validator block")
+
+	//attack v1&v2 against block
+	c5 := committeeSet.GetProposer(1, big.NewInt(4))
+	t.Log("round ", 1, "height", 4, c5, "maligous validator block")
+}
+
+func TestWeightedRoundRobinProposerValidatorsAttackVector2(t *testing.T) {
+	b1 := common.Hex2Bytes(validator1)
+	b2 := common.Hex2Bytes(validator2)
+	b3 := common.Hex2Bytes(validator3)
+	b4 := common.Hex2Bytes(validator4)
+	addr1 := common.BytesToAddress(b1)
+	addr2 := common.BytesToAddress(b2)
+	addr3 := common.BytesToAddress(b3)
+	addr4 := common.BytesToAddress(b4)
+	val1 := types.CommitteeMember{Address: addr1, VotingPower: new(big.Int).SetUint64(50)}
+	val2 := types.CommitteeMember{Address: addr2, VotingPower: new(big.Int).SetUint64(40)}
+	val3 := types.CommitteeMember{Address: addr3, VotingPower: new(big.Int).SetUint64(100)}
+	val4 := types.CommitteeMember{Address: addr4, VotingPower: new(big.Int).SetUint64(200)}
+	committeeSet, err := NewSet(types.Committee{val1, val2, val3, val4}, config.WeightedRoundRobin, val1.Address)
+	if committeeSet == nil || err != nil {
+		t.Errorf("the format of validator set is invalid")
+		t.FailNow()
+	}
+	h := int64(1)
+	r := int64(1)
+	for i := 0; i < 100; i++ {
+		c := committeeSet.GetProposer(r, big.NewInt(h))
+		if c.Address == addr1 || c.Address == addr2 {
+			t.Log("round ", r, "height", h, c, "maligous validator block")
+			h++
+			r = 1
+		} else {
+			t.Log("round ", r, "height", h, c, "vote agains it")
+			r++
+		}
+	}
 }
 
 func testWRRDetermination(t *testing.T) {
