3bb60c40a1e4757ba8312624cb456d3387628639
Piers Powlesland <pierspowlesland@gmail.com>
2020-05-20 18:51:33 +0100

    Add common functionality to types.Header
    The logic for iterating over Header.Committee was being duplicated in at
    least 3 places, so centralised the logic in the Header type

--- core/types/autonity_header_methods.go
+++ core/types/autonity_header_methods.go
@@ -1,5 +1,20 @@
 package types
 
+import "github.com/clearmatics/autonity/common"
+
 func (h *Header) IsGenesis() bool {
 	return h.Number.Uint64() == 0
+}
+
+// CommitteeMember returns the committee member having the given address or
+// nil if there is none.
+func (h *Header) CommitteeMember(address common.Address) *CommitteeMember {
+	if h.committeeMap == nil {
+		h.committeeMap = make(map[common.Address]*CommitteeMember)
+		for i := range h.Committee {
+			member := h.Committee[i]
+			h.committeeMap[member.Address] = &member
+		}
+	}
+	return h.committeeMap[address]
 }
