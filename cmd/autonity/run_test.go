7c68eb3fbd5e398596e4e78fc49472a85f6bc78f
Piers Powlesland <pierspowlesland@gmail.com>
2020-06-01 19:29:02 +0100

    Attempt to init the test exe
    This is not working, I'm not sure how to get it to work.
    The tests that are failing are very badly made. I can't see a way to get
    this to work without rewriting them all and changing the way the tests
    are executed to be more sane.

--- cmd/autonity/run_test.go
+++ cmd/autonity/run_test.go
@@ -49,6 +49,13 @@ type testautonity struct {
 func init() {
 	// Run the app if we've been exec'd as "autonity-test" in runAutonity.
 	reexec.Register("autonity-test", func() {
+
+		if len(os.Args) > 3 {
+			if err := app.Run([]string{"autonity-test", os.Args[1], os.Args[2], "init", "../../helloworld/genesis-tendermint.json"}); err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+		}
 		if err := app.Run(os.Args); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
