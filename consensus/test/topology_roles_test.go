35ec5138f8d51f622bc2909c2f09b7fb030152ee
Piers Powlesland <pierspowlesland@gmail.com>
2020-07-20 16:23:04 +0100

    Skipped test that is failing intermittently
    To be fixed in https://github.com/clearmatics/autonity/issues/619

--- consensus/test/topology_roles_test.go
+++ consensus/test/topology_roles_test.go
@@ -92,6 +92,7 @@ func TestTendermintMajorityExternalUsers(t *testing.T) {
 }
 
 func TestTendermintMajorityExternalUsersFullyConnected(t *testing.T) {
+	t.Skip("This test is intermittently failing, see - https://github.com/clearmatics/autonity/issues/619")
 	if testing.Short() {
 		t.Skip("skipping test in short mode")
 	}
