20317f7a611097b462b26040f3b4709fa3a00b16
Jason.Chen <jason.chen@clearmatics.com>
2020-10-06 05:35:05 +0100

    Reopen skipped testcase on topology_test.go

--- consensus/test/topology_test.go
+++ consensus/test/topology_test.go
@@ -43,7 +43,6 @@ func TestTendermintStarSuccess(t *testing.T) {
 }
 
 func TestTendermintStarOverParticipantSuccess(t *testing.T) {
-	t.Skip("test is flaky - https://github.com/clearmatics/autonity/issues/496")
 	if testing.Short() {
 		t.Skip("skipping test in short mode")
 	}
