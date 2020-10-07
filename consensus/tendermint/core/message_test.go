0c25163942eb59dc68c849b79ad007c75e4e8287
Jason.Chen <jason.chen@clearmatics.com>
2020-10-07 10:29:33 +0100

    Reslove review comments from piers: remove testing of string representation.

--- consensus/tendermint/core/message_test.go
+++ consensus/tendermint/core/message_test.go
@@ -41,25 +41,6 @@ func TestMessageEncodeDecode(t *testing.T) {
 	}
 }
 
-func TestMessageString(t *testing.T) {
-	for i := 0; i < 3; i++ {
-		msg := &Message{
-			Code:          uint64(i),
-			Address:       common.HexToAddress("0x1234567890"),
-			Msg:           []byte{0x23, 0x44},
-			Signature:     []byte{0x24, 0x43},
-			CommittedSeal: []byte{0x22, 0x33},
-			power:         100,
-		}
-
-		// decode error would return ""
-		want := ""
-		if got := msg.String(); got != want {
-			t.Errorf("Expected %v, got %v", want, got)
-		}
-	}
-}
-
 func TestMessageValidate(t *testing.T) {
 
 	t.Run("validate function fails, error returned", func(t *testing.T) {
