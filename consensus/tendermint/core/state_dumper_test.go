cb17a3de6d033d5c79b4da0e38dbf356ed19a3cf
piersy <pierspowlesland@gmail.com>
2020-10-29 06:40:44 +0000

    Separate out state dump tests (#714)
    * Separate out state dump tests
    
    The tests as they were shared a huge context (in the sense of variables
    and closures in scope) and that made it very hard to see what each test
    was actually using out of the common context. This commit moves each
    test to a separate test which dramatically shrunk the context for most
    of the tests making them much easier to follow.
    
    * Ignore broken test
    
    Co-authored-by: Jason Chen <54585152+Jason-Zhangxin-Chen@users.noreply.github.com>

--- consensus/tendermint/core/state_dumper_test.go
+++ consensus/tendermint/core/state_dumper_test.go
@@ -1,216 +1,202 @@
 package core
 
 import (
-	"crypto/ecdsa"
-	"github.com/clearmatics/autonity/common"
-	"github.com/clearmatics/autonity/consensus/tendermint/config"
-	"github.com/clearmatics/autonity/core/types"
-	"github.com/golang/mock/gomock"
-	"github.com/stretchr/testify/require"
-	"gotest.tools/assert"
 	"math/big"
 	"math/rand"
 	"testing"
 	"time"
+
+	"github.com/clearmatics/autonity/common"
+	"github.com/clearmatics/autonity/consensus/tendermint/config"
+	"github.com/clearmatics/autonity/core/types"
+	"github.com/clearmatics/autonity/crypto"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestStateDumper(t *testing.T) {
-	committeeSizeAndMaxRound := rand.Intn(maxSize-minSize) + minSize
-	committeeSet, privateKeys := prepareCommittee(t, committeeSizeAndMaxRound)
-	members := committeeSet.Committee()
-	clientAddr := members[0].Address
+func TestGetLockedValueAndValidValue(t *testing.T) {
+	c := &core{}
+	b := generateBlock(new(big.Int).SetUint64(1))
+	c.lockedValue = b
+	c.validValue = b
 
-	genRoundVoteMessages := func(c *core, round int64, proposal *Proposal, proposalMsg *Message, preVoteMsg *Message, preCommitMsg *Message, verified bool) {
-		c.messages.getOrCreate(round).SetProposal(proposal, proposalMsg, verified)
-		c.messages.getOrCreate(round).AddPrevote(proposal.ProposalBlock.Hash(), *preVoteMsg)
-		c.messages.getOrCreate(round).AddPrecommit(proposal.ProposalBlock.Hash(), *preCommitMsg)
+	assert.Equal(t, c.lockedValue.Hash(), *getHash(c.lockedValue))
+	assert.Equal(t, c.validValue.Hash(), *getHash(c.validValue))
+}
+
+func TestGetProposal(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	nodeAddr := common.BytesToAddress([]byte("node"))
+	backendMock := NewMockBackend(ctrl)
+	backendMock.EXPECT().Address().Return(nodeAddr)
+	core := New(backendMock, config.RoundRobinConfig())
+
+	proposalMsg, proposal := randomProposal(t)
+	core.messages.getOrCreate(proposal.Round).SetProposal(&proposal, proposalMsg, true)
+
+	assert.Equal(t, proposal.ProposalBlock.Hash(), *getProposal(core, proposal.Round))
+}
+
+func TestGetRoundState(t *testing.T) {
+	sender := common.BytesToAddress([]byte("sender"))
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	backendMock := NewMockBackend(ctrl)
+	backendMock.EXPECT().Address().Return(sender)
+
+	c := New(backendMock, config.DefaultConfig())
+
+	var rounds []int64 = []int64{0, 1}
+	height := big.NewInt(int64(100) + 1)
+
+	// Prepare 2 rounds of messages
+	proposals := make([]Proposal, 2)
+	proposals[0], _ = prepareRoundMsgs(t, c, rounds[0], height, sender)
+	proposals[1], _ = prepareRoundMsgs(t, c, rounds[1], height, sender)
+
+	// Get the states
+	states := getRoundState(c)
+
+	// expect 2 rounds of vote states.
+	require.Len(t, states, 2)
+	for _, state := range states {
+		assert.Contains(t, rounds, state.Round)
+		checkRoundState(t, state, rounds[state.Round], &proposals[state.Round], true)
 	}
+}
 
-	checkRoundState := func(t *testing.T, s RoundState, wantRound int64, wantProposal *Proposal, wantVerfied bool) {
-		require.Equal(t, wantProposal.ProposalBlock.Hash(), s.Proposal)
-		require.Len(t, s.PrevoteState, 1)
-		require.Len(t, s.PrecommitState, 1)
-		require.Equal(t, wantRound, s.Round)
+func TestGetCoreState(t *testing.T) {
+	height := big.NewInt(int64(100) + 1)
+	prevHeight := height.Sub(height, big.NewInt(1))
+	prevBlock := generateBlock(prevHeight)
+	knownMsgHash := []common.Hash{{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5}, {0, 0, 1, 3, 6}}
+	sender := common.BytesToAddress([]byte("sender"))
 
-		require.Equal(t, wantVerfied, s.PrevoteState[0].ProposalVerified)
-		require.Equal(t, wantProposal.ProposalBlock.Hash(), s.PrevoteState[0].Value)
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-		require.Equal(t, wantVerfied, s.PrecommitState[0].ProposalVerified)
-		require.Equal(t, wantProposal.ProposalBlock.Hash(), s.PrecommitState[0].Value)
-	}
+	backendMock := NewMockBackend(ctrl)
+	backendMock.EXPECT().Address().Return(sender)
+	backendMock.EXPECT().KnownMsgHash().Return(knownMsgHash)
 
-	prepareRoundMsgs := func(t *testing.T, r int64, h *big.Int, vr int64, proposer common.Address, sender common.Address,
-		privKey *ecdsa.PrivateKey) (*Message, Proposal, *Message, *Message) {
-		proposalMsg, proposal := generateBlockProposal(t, r, h, vr, proposer, false)
-		prevoteMsg, _, _ := prepareVote(t, msgPrevote, r, h, proposal.ProposalBlock.Hash(), sender, privKey)
-		precommitMsg, _, _ := prepareVote(t, msgPrecommit, r, h, proposal.ProposalBlock.Hash(), sender, privKey)
-		return proposalMsg, proposal, prevoteMsg, precommitMsg
-	}
+	c := New(backendMock, config.DefaultConfig())
 
-	setCoreState := func(c *core, h *big.Int, r int64, s Step, lv *types.Block, lr int64, vv *types.Block, vr int64, committee committee, header *types.Header) {
-		c.setHeight(h)
-		c.setRound(r)
-		c.setStep(s)
-		c.lockedValue = lv
-		c.lockedRound = lr
-		c.validValue = vv
-		c.validRound = vr
-		c.setCommitteeSet(committee)
-		c.lastHeader = header
-		c.sentProposal = true
-		c.sentPrevote = true
-		c.sentPrecommit = true
-		c.setValidRoundAndValue = true
-	}
+	var rounds []int64 = []int64{0, 1}
 
-	checkState := func(t *testing.T, c *core, state TendermintState, currentHeight *big.Int, currentRound int64, initRound int64,
-		initProposal Proposal, newProposal Proposal, knownMsgHash []common.Hash) {
+	// Prepare 2 rounds of messages
+	proposals := make([]Proposal, 2)
+	proposers := make([]common.Address, 2)
+	proposals[0], proposers[0] = prepareRoundMsgs(t, c, rounds[0], height, sender)
+	proposals[1], proposers[1] = prepareRoundMsgs(t, c, rounds[1], height, sender)
 
-		require.Equal(t, clientAddr, state.Client)
-		require.Equal(t, uint64(c.proposerPolicy), state.ProposerPolicy)
-		require.Equal(t, c.blockPeriod, state.BlockPeriod)
-		require.Len(t, state.CurHeightMessages, 6)
-		require.Equal(t, currentHeight, state.Height)
-		require.Equal(t, currentRound, state.Round)
-		require.Equal(t, uint64(propose), state.Step)
-		require.Equal(t, newProposal.ProposalBlock.Hash(), *state.Proposal)
-		require.Equal(t, initProposal.ProposalBlock.Hash(), *state.LockedValue)
-		require.Equal(t, initRound, state.LockedRound)
-		require.Equal(t, initProposal.ProposalBlock.Hash(), *state.ValidValue)
-		require.Equal(t, initRound, state.ValidRound)
-		require.Equal(t, committeeSet.Committee().String(), state.Committee.String())
-		require.Equal(t, members[currentRound].Address, state.Proposer)
-		require.False(t, state.IsProposer)
-		require.Equal(t, committeeSet.Quorum(), state.QuorumVotePower)
-		require.True(t, state.SentProposal)
-		require.True(t, state.SentPrevote)
-		require.True(t, state.SentPrecommit)
-		require.True(t, state.SetValidRoundAndValue)
-		require.False(t, state.ProposeTimerStarted)
-		require.False(t, state.PrevoteTimerStarted)
-		require.False(t, state.PrecommitTimerStarted)
-		require.Equal(t, knownMsgHash, state.KnownMsgHash)
+	one := common.Big1
+	members := []types.CommitteeMember{{Address: proposers[0], VotingPower: one}, {Address: proposers[1], VotingPower: one}}
+	committeeSet, err := newRoundRobinSet(members, proposers[1]) // todo construct set here
+	require.NoError(t, err)
+	setCoreState(c, height, rounds[1], propose, proposals[0].ProposalBlock, rounds[0], proposals[0].ProposalBlock, rounds[0], committeeSet,
+		prevBlock.Header())
 
-		// expect 2 rounds of vote states.
-		require.Len(t, state.RoundStates, 2)
-		for _, v := range state.RoundStates {
-			require.Contains(t, []int64{initRound, currentRound}, v.Round)
-			if v.Round == currentRound {
-				checkRoundState(t, v, currentRound, &newProposal, true)
-			}
-			if v.Round == initRound {
-				checkRoundState(t, v, initRound, &initProposal, true)
-			}
-		}
-	}
-
-	t.Run("get proposal, locked value and valid value", func(t *testing.T) {
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-		backendMock := NewMockBackend(ctrl)
-		backendMock.EXPECT().Address().Return(clientAddr)
-
-		core := New(backendMock, config.RoundRobinConfig())
-		currentHeight := big.NewInt(int64(rand.Intn(maxSize) + 1))
-		currentRound := int64(rand.Intn(committeeSizeAndMaxRound))
-		proposalMsg, proposal := generateBlockProposal(t, currentRound, currentHeight, int64(rand.Intn(int(currentRound+1)-1)), members[currentRound].Address, false)
-		core.messages.getOrCreate(currentRound).SetProposal(&proposal, proposalMsg, true)
-		fact := getProposal(core, currentRound)
-		core.lockedValue = proposal.ProposalBlock
-		core.validValue = proposal.ProposalBlock
-		assert.Equal(t, proposal.ProposalBlock.Hash(), *fact)
-		assert.Equal(t, proposal.ProposalBlock.Hash(), core.lockedValue.Hash())
-		assert.Equal(t, proposal.ProposalBlock.Hash(), core.validValue.Hash())
-	})
-
-	t.Run("get round state", func(t *testing.T) {
-
-		currentHeight := big.NewInt(int64(rand.Intn(maxSize) + 1))
-		// round 0 messages
-		initRound := int64(0)
-		initProposalMsg, initProposal, initRoundPrevoteMsg, initRoundPrecommitMsg := prepareRoundMsgs(t, initRound, currentHeight, 0, members[initRound].Address, clientAddr, privateKeys[clientAddr])
-
-		// round 1 messages
-		currentRound := int64(1)
-		sender := 1
-		newProposalMsg, newProposal, curRoundPrevoteMsg, curRoundPrecommitMsg := prepareRoundMsgs(t, currentRound, currentHeight, 0, members[currentRound].Address, members[sender].Address, privateKeys[members[sender].Address])
-
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-
-		backendMock := NewMockBackend(ctrl)
-		backendMock.EXPECT().Address().Return(clientAddr)
-
-		c := New(backendMock, config.DefaultConfig())
-
-		// round 0 messages:
-		genRoundVoteMessages(c, initRound, &initProposal, initProposalMsg, initRoundPrevoteMsg, initRoundPrecommitMsg, true)
-		// current round messages:
-		genRoundVoteMessages(c, currentRound, &newProposal, newProposalMsg, curRoundPrevoteMsg, curRoundPrecommitMsg, true)
-
-		states := getRoundState(c)
-
-		// expect 2 rounds of vote states.
-		require.Len(t, states, 2)
-		for _, v := range states {
-			require.Contains(t, []int64{initRound, currentRound}, v.Round)
-			if v.Round == currentRound {
-				checkRoundState(t, v, currentRound, &newProposal, true)
-			}
-			if v.Round == initRound {
-				checkRoundState(t, v, initRound, &initProposal, true)
-			}
-		}
-	})
-
-	t.Run("test dump state handler", func(t *testing.T) {
-		currentHeight := big.NewInt(int64(rand.Intn(maxSize) + 1))
-		prevHeight := currentHeight.Sub(currentHeight, big.NewInt(1))
-		prevBlock := generateBlock(prevHeight)
-		knownMsgHash := []common.Hash{{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5}, {0, 0, 1, 3, 6}}
-
-		// round 0 messages
-		initRound := int64(0)
-		initProposalMsg, initProposal, initRoundPrevoteMsg, initRoundPrecommitMsg := prepareRoundMsgs(t, initRound, currentHeight, 0, members[initRound].Address, clientAddr, privateKeys[clientAddr])
-
-		// round 1 messages
-		currentRound := int64(1)
-		sender := 1
-		newProposalMsg, newProposal, curRoundPrevoteMsg, curRoundPrecommitMsg := prepareRoundMsgs(t, currentRound, currentHeight, 0, members[currentRound].Address, members[sender].Address, privateKeys[members[sender].Address])
-
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-
-		backendMock := NewMockBackend(ctrl)
-		backendMock.EXPECT().Address().Return(clientAddr)
-		backendMock.EXPECT().KnownMsgHash().Return(knownMsgHash)
-
-		c := New(backendMock, config.DefaultConfig())
-		c.address = clientAddr
-		// round 0 messages:
-		genRoundVoteMessages(c, initRound, &initProposal, initProposalMsg, initRoundPrevoteMsg, initRoundPrecommitMsg, true)
-		// current round messages:
-		genRoundVoteMessages(c, currentRound, &newProposal, newProposalMsg, curRoundPrevoteMsg, curRoundPrecommitMsg, true)
-
-		setCoreState(c, currentHeight, currentRound, propose, initProposal.ProposalBlock, initRound, initProposal.ProposalBlock, initRound, committeeSet,
-			prevBlock.Header())
-
-		var e = coreStateRequestEvent{
+  var e = coreStateRequestEvent{
 			stateChan: make(chan TendermintState),
-		}
+	}
+	go c.handleStateDump(e)
+  state := <-e.stateChan
+  
+	assert.Equal(t, sender, state.Client)
+	assert.Equal(t, uint64(c.proposerPolicy), state.ProposerPolicy)
+	assert.Equal(t, c.blockPeriod, state.BlockPeriod)
+	assert.Len(t, state.CurHeightMessages, 6)
+	assert.Equal(t, height, state.Height)
+	assert.Equal(t, rounds[1], state.Round)
+	assert.Equal(t, uint64(propose), state.Step)
+	assert.Equal(t, proposals[1].ProposalBlock.Hash(), *state.Proposal)
+	assert.Equal(t, proposals[0].ProposalBlock.Hash(), *state.LockedValue)
+	assert.Equal(t, rounds[0], state.LockedRound)
+	assert.Equal(t, proposals[0].ProposalBlock.Hash(), *state.ValidValue)
+	assert.Equal(t, rounds[0], state.ValidRound)
+	assert.Equal(t, committeeSet.Committee().String(), state.Committee.String())
+	assert.Equal(t, committeeSet.GetProposer(rounds[1]).Address, state.Proposer)
+	assert.False(t, state.IsProposer)
+	assert.Equal(t, committeeSet.Quorum(), state.QuorumVotePower)
+	assert.True(t, state.SentProposal)
+	assert.True(t, state.SentPrevote)
+	assert.True(t, state.SentPrecommit)
+	assert.True(t, state.SetValidRoundAndValue)
+	assert.False(t, state.ProposeTimerStarted)
+	assert.False(t, state.PrevoteTimerStarted)
+	assert.False(t, state.PrecommitTimerStarted)
+	assert.Equal(t, knownMsgHash, state.KnownMsgHash)
 
-		go c.handleStateDump(e)
+	// expect 2 rounds of vote states.
+	require.Len(t, state.RoundStates, 2)
+	for _, s := range state.RoundStates {
+		assert.Contains(t, rounds, s.Round)
+		checkRoundState(t, s, rounds[s.Round], &proposals[s.Round], true)
+	}
 
-		state := TendermintState{}
-		// wait for response with timeout.
-		timeout := time.After(time.Second)
-		select {
-		case s := <-e.stateChan:
-			state = s
-		case <-timeout:
-			t.Fatal("fetch tendermint state time out")
-		}
+}
 
-		checkState(t, c, state, currentHeight, currentRound, initRound, initProposal, newProposal, knownMsgHash)
-	})
+func TestDumpStateTimeout(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	backendMock := NewMockBackend(ctrl)
+	backendMock.EXPECT().Address().Return(common.BytesToAddress([]byte("address")))
+	backendMock.EXPECT().Post(coreStateRequestEvent{}).Times(1)
+	core := New(backendMock, config.DefaultConfig())
+
+	state := core.CoreState()
+	require.Equal(t, int64(-1), state.Code)
+}
+
+func randomProposal(t *testing.T) (*Message, Proposal) {
+	currentHeight := big.NewInt(int64(rand.Intn(100) + 1))
+	currentRound := int64(rand.Intn(100) + 1)
+
+	proposer := common.BytesToAddress([]byte("proposer"))
+	return generateBlockProposal(t, currentRound, currentHeight, currentRound-1, proposer, false)
+}
+
+func checkRoundState(t *testing.T, s RoundState, wantRound int64, wantProposal *Proposal, wantVerfied bool) {
+	require.Equal(t, wantProposal.ProposalBlock.Hash(), s.Proposal)
+	require.Len(t, s.PrevoteState, 1)
+	require.Len(t, s.PrecommitState, 1)
+	require.Equal(t, wantRound, s.Round)
+
+	require.Equal(t, wantVerfied, s.PrevoteState[0].ProposalVerified)
+	require.Equal(t, wantProposal.ProposalBlock.Hash(), s.PrevoteState[0].Value)
+
+	require.Equal(t, wantVerfied, s.PrecommitState[0].ProposalVerified)
+	require.Equal(t, wantProposal.ProposalBlock.Hash(), s.PrecommitState[0].Value)
+}
+
+func prepareRoundMsgs(t *testing.T, c *core, r int64, h *big.Int, sender common.Address) (proposal Proposal, proposer common.Address) {
+	privKey, err := crypto.GenerateKey()
+	require.NoError(t, err)
+	proposalMsg, proposal := generateBlockProposal(t, r, h, 0, crypto.PubkeyToAddress(privKey.PublicKey), false)
+	prevoteMsg, _, _ := prepareVote(t, msgPrevote, r, h, proposal.ProposalBlock.Hash(), sender, privKey)
+	precommitMsg, _, _ := prepareVote(t, msgPrecommit, r, h, proposal.ProposalBlock.Hash(), sender, privKey)
+	c.messages.getOrCreate(r).SetProposal(&proposal, proposalMsg, true)
+	c.messages.getOrCreate(r).AddPrevote(proposal.ProposalBlock.Hash(), *prevoteMsg)
+	c.messages.getOrCreate(r).AddPrecommit(proposal.ProposalBlock.Hash(), *precommitMsg)
+	return proposal, proposalMsg.Address
+}
+
+func setCoreState(c *core, h *big.Int, r int64, s Step, lv *types.Block, lr int64, vv *types.Block, vr int64, committee committee, header *types.Header) {
+	c.setHeight(h)
+	c.setRound(r)
+	c.setStep(s)
+	c.lockedValue = lv
+	c.lockedRound = lr
+	c.validValue = vv
+	c.validRound = vr
+	c.setCommitteeSet(committee)
+	c.lastHeader = header
+	c.sentProposal = true
+	c.sentPrevote = true
+	c.sentPrecommit = true
+	c.setValidRoundAndValue = true
 }
