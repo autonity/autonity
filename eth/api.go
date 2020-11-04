1f95ca25d6ff433c101a8802f260fc89c3c393ac
Piers Powlesland <pierspowlesland@gmail.com>
2020-11-04 15:57:45 +0000

    Manually unpack element from results of length 1

--- eth/api.go
+++ eth/api.go
@@ -602,6 +602,12 @@ func NewAutonityContractAPI(bc *core.BlockChain, ac *autonity.Contract) *Autonit
 						return makereturn(nil, err)
 					}
 					result, err := contractABI.Unpack(functionName, packedResult)
+
+					// If the result is a slice with one element only then just return the element.
+					v := reflect.ValueOf(result)
+					if v.Len() == 1 {
+						return makereturn(v.Index(0).Interface(), err)
+					}
 					return makereturn(result, err)
 				})
 		}
