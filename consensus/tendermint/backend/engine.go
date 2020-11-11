3c0a5b5809a352752acd226940a0d65957655058
Piers Powlesland <pierspowlesland@gmail.com>
2020-11-11 18:02:31 +0000

    Revert "Panic if result channel changed"
    This reverts commit 79e4a81e796e390cdaea11f1b86de3dea1446cce.

--- consensus/tendermint/backend/engine.go
+++ consensus/tendermint/backend/engine.go
@@ -141,9 +141,6 @@ func (sb *Backend) setResultChan(results chan<- *types.Block) {
 	sb.coreMu.Lock()
 	defer sb.coreMu.Unlock()
 
-	if sb.commitCh != nil || sb.commitCh != results {
-		panic("setting result channel to different channel")
-	}
 	sb.commitCh = results
 }
 
