3b4b4cd0d33fc2dd655b3424f19d7d4625c88d69
Thomas Modeneis <thomas.modeneis@gmail.com>
2020-12-17 22:55:54 +0100

    add err check for sb.Finalize that was un-checked potentially causing issues

--- consensus/tendermint/backend/backend.go
+++ consensus/tendermint/backend/backend.go
@@ -325,6 +325,10 @@ func (sb *Backend) VerifyProposal(proposal types.Block) (time.Duration, error) {
 
 		state.Prepare(common.ACHash(block.Number()), block.Hash(), len(block.Transactions()))
 		committeeSet, receipt, err := sb.Finalize(sb.blockchain, header, state, block.Transactions(), nil, receipts)
+		if err != nil {
+			sb.logger.Error("Error when calling Finalize", "err", err)
+			return 0, err
+		}
 		receipts = append(receipts, receipt)
 		//Validate the state of the proposal
 		if err = sb.blockchain.Validator().ValidateState(block, state, receipts, *usedGas); err != nil {
