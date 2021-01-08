4d4a559776beaba1efe52be70b1715cbac3546b9
Piers Powlesland <pierspowlesland@gmail.com>
2021-01-08 17:48:42 +0000

    Log time waited in block awaiter

--- consensus/tendermint/blockAwaiter.go
+++ consensus/tendermint/blockAwaiter.go
@@ -2,6 +2,7 @@ package tendermint
 
 import (
 	"sync"
+	time "time"
 
 	"github.com/clearmatics/autonity/core/types"
 )
@@ -33,24 +34,26 @@ func (a *blockAwaiter) setValue(b *types.Block) {
 // value will return the lastValue set by setValue for the current height. If lastValue is nil or is of a previous
 // height then the function will wait until setValue is called or signal to quit is received.
 func (a *blockAwaiter) value(height uint64) (*types.Block, error) {
+	start := time.Now()
 	a.valueCond.L.Lock()
 	defer a.valueCond.L.Unlock()
 
 	a.dlog.print("beginning awaiting value", height)
 	for {
+		secondsWaited := time.Now().Sub(start) / time.Second
 		if a.stopped {
 			return nil, errStopped
 		}
 		if a.lastValue == nil || a.lastValue.Number().Uint64() != height {
 			if a.lastValue == nil {
-				a.dlog.print("awaiting value", "valueIsNil", "awaited height", height)
+				a.dlog.print("awaiting value", "valueIsNil", "awaited height", height, "waiting for", secondsWaited)
 			} else {
-				a.dlog.print("awaiting value", a.lastValue.Hash().String()[2:8], "value height", a.lastValue.Number().String(), "awaited height", height)
+				a.dlog.print("awaiting value", a.lastValue.Hash().String()[2:8], "value height", a.lastValue.Number().String(), "awaited height", height, "waiting for", secondsWaited)
 			}
 			a.lastValue = nil
 			a.valueCond.Wait()
 		} else {
-			a.dlog.print("received awaited value", a.lastValue.Hash().String()[2:8], "value height", a.lastValue.Number().String(), "awaited height", height)
+			a.dlog.print("received awaited value", a.lastValue.Hash().String()[2:8], "value height", a.lastValue.Number().String(), "awaited height", height, "waited for", secondsWaited)
 			return a.lastValue, nil
 		}
 	}
