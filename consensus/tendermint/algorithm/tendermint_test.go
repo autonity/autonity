657054f2db0c7125caf853169798e621e181ad4f
Piers Powlesland <pierspowlesland@gmail.com>
2021-01-11 10:16:43 +0000

    Expand test cases for StartRound

--- consensus/tendermint/algorithm/tendermint_test.go
+++ consensus/tendermint/algorithm/tendermint_test.go
@@ -2,6 +2,7 @@ package algorithm
 
 import (
 	"crypto/rand"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,15 +16,24 @@ func newNodeId(t *testing.T) NodeID {
 	return nodeID
 }
 
+func newValue(t *testing.T) ValueID {
+	var value ValueID
+	_, err := rand.Read(value[:])
+	require.NoError(t, err)
+	return value
+}
+
 func TestStartRound(t *testing.T) {
 	proposer := newNodeId(t)
-	// node := newNodeId(t)
 	var round int64 = 0
+
 	o := &mockOracle{
 		proposer: func(round int64, nodeID NodeID) bool {
 			return nodeID == proposer
 		},
 	}
+
+	// We are proposer, expect propose message
 	algo := New(proposer, o)
 	cm, to, err := algo.StartRound(round)
 	assert.Nil(t, err)
@@ -36,6 +46,43 @@ func TestStartRound(t *testing.T) {
 		ValidRound: -1,
 	}
 	assert.Equal(t, expected, cm)
+
+	// We are proposer, and validValue has been set, expect propose with
+	// validValue.
+	algo = New(proposer, o)
+	algo.validValue = newValue(t)
+	cm, to, err = algo.StartRound(round)
+	assert.Nil(t, err)
+	assert.Nil(t, to)
+	expected = &ConsensusMessage{
+		MsgType:    Propose,
+		Height:     o.Height(),
+		Round:      round,
+		Value:      algo.validValue,
+		ValidRound: -1,
+	}
+	assert.Equal(t, expected, cm)
+
+	// We are proposer but oracle value returns an error, expect the error
+	o.valueError = errors.New("")
+	algo = New(proposer, o)
+	cm, to, err = algo.StartRound(round)
+	assert.Nil(t, cm)
+	assert.Nil(t, to)
+	assert.Error(t, err)
+
+	// We are not the proposer, expect timeout message
+	algo = New(newNodeId(t), o)
+	cm, to, err = algo.StartRound(round)
+	assert.Nil(t, err)
+	assert.Nil(t, cm)
+	expectedTimeout := &Timeout{
+		TimeoutType: Propose,
+		Delay:       1,
+		Height:      o.Height(),
+		Round:       round,
+	}
+	assert.Equal(t, expectedTimeout, to)
 
 }
 
