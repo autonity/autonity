b9825476960a4501d3bfa720e2c39402f8e6eb70
Jason.Chen <jason.chen@clearmatics.com>
2021-01-15 09:48:44 +0000

    Add innocent prover and tx_sender object for fault detector.

--- consensus/tendermint/faultdetector/tx_sender.go
+++ consensus/tendermint/faultdetector/tx_sender.go
@@ -1,1 +1,22 @@
 package faultdetector
+
+import (
+	"crypto"
+	"github.com/clearmatics/autonity/common"
+)
+
+type TXSender struct {
+	contract common.Address // autonity contract address
+	abi string // cotnract abi
+	key crypto.PrivateKey // private key of etherbase
+}
+
+func (s *TXSender) SendSuspicion(proof *Proof) common.Hash {
+	// todo send suspicoin and return a TX hash.
+	return common.Hash{}
+}
+
+func (s *TXSender) SendInnocentProof(proof *InnocentProof) common.Hash {
+	// todo send innocent proof and return a TX hash.
+	return common.Hash{}
+}
