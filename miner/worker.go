ab830f464b60391bae6034ca950e29a7bd7c074a
Piers Powlesland <pierspowlesland@gmail.com>
2021-01-18 22:23:04 +0000

    Wait for worker goroutines to complete

--- miner/worker.go
+++ miner/worker.go
@@ -25,6 +25,8 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/piersy/goroutine-waiter/gwait"
+
 	"github.com/clearmatics/autonity/common"
 	"github.com/clearmatics/autonity/consensus"
 	"github.com/clearmatics/autonity/consensus/misc"
@@ -151,6 +153,9 @@ type worker struct {
 	resubmitIntervalCh chan time.Duration
 	resubmitAdjustCh   chan *intervalAdjust
 
+	// Go routine waiter
+	waiter *gwait.Waiter
+
 	current      *environment                 // An environment for current running cycle.
 	localUncles  map[common.Hash]*types.Block // A set of side blocks generated locally as the possible uncle blocks.
 	remoteUncles map[common.Hash]*types.Block // A set of side blocks as the possible uncle blocks.
@@ -211,6 +216,7 @@ func newWorker(config *Config, chainConfig *params.ChainConfig, engine consensus
 		startCh:            make(chan struct{}, 1),
 		resubmitIntervalCh: make(chan time.Duration),
 		resubmitAdjustCh:   make(chan *intervalAdjust, resubmitAdjustChanSize),
+		waiter:             gwait.NewWaiter(),
 	}
 	// Subscribe NewTxsEvent for tx pool
 	worker.txsSub = eth.TxPool().SubscribeNewTxsEvent(worker.txsCh)
@@ -225,10 +231,10 @@ func newWorker(config *Config, chainConfig *params.ChainConfig, engine consensus
 		recommit = minRecommitInterval
 	}
 
-	go worker.mainLoop()
-	go worker.newWorkLoop(recommit)
-	go worker.resultLoop()
-	go worker.taskLoop()
+	worker.waiter.Go(worker.mainLoop)
+	worker.waiter.Go(func() { worker.newWorkLoop(recommit) })
+	worker.waiter.Go(worker.resultLoop)
+	worker.waiter.Go(worker.taskLoop)
 
 	// Submit first work to initialize pending state.
 	if init {
@@ -316,6 +322,7 @@ func (w *worker) isRunning() bool {
 func (w *worker) close() {
 	atomic.StoreInt32(&w.running, 0)
 	close(w.exitCh)
+	w.waiter.Wait()
 }
 
 // recalcRecommit recalculates the resubmitting interval upon feedback.
