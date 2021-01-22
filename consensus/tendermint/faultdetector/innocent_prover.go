e5c4db4a2127b2a109717a2192708d500f224f97
Jason.Chen <jason.chen@clearmatics.com>
2021-01-22 13:22:03 +0000

    Check if evidence messages were sent from correct commmittee in precompiled contracts.

--- consensus/tendermint/faultdetector/innocent_prover.go
+++ consensus/tendermint/faultdetector/innocent_prover.go
@@ -1,17 +1,39 @@
 package faultdetector
 
+import (
+	"fmt"
+	"github.com/clearmatics/autonity/core"
+)
+
 // validate the proof is a valid challenge.
-func validateChallenge(c *Proof) error {
-	// todo: check if messages are signed by correct committee member of its round. AFD should buffer N block headers at package level,
-	// otherwise static precompiled contract in evm context cannot validate whether messages are from correct committee member.
+func validateChallenge(c *Proof, chain core.ChainContext) error {
+	// get committee from block header
+	header := chain.GetHeader(c.parentHash, uint64(c.Message.Height()-1))
+
+	// check if evidences senders are presented in committee.
+	for i:=0; i < len(c.Evidence); i++ {
+		member := header.CommitteeMember(c.Evidence[i].Sender())
+		if member == nil {
+			return fmt.Errorf("invalid evidence for proof of susipicous message")
+		}
+	}
+
 	// todo: check if the suspicious message is proved by given evidence as a valid suspicion.
 	return nil
 }
 
 // validate the innocent proof is valid.
-func validateInnocentProof(i *Proof) error {
-	// todo: check if messages are signed by correct committee member of its round. AFD should buffer N block headers at package level,
-	// otherwise static precompiled contract in evm context cannot validate whether messages are from correct committee member.
+func validateInnocentProof(in *Proof, chain core.ChainContext) error {
+	// get committee from block header
+	header := chain.GetHeader(in.parentHash, uint64(in.Message.Height()-1))
+
+	// check if evidences senders are presented in committee.
+	for i:=0; i < len(in.Evidence); i++ {
+		member := header.CommitteeMember(in.Evidence[i].Sender())
+		if member == nil {
+			return fmt.Errorf("invalid evidence for proof of susipicous message")
+		}
+	}
 	// todo: check if the suspicious message is proved by given evidence as an innocent behavior.
 	return nil
 }
@@ -23,12 +45,12 @@ func resolveChallenge(c *Proof) (innocentProof *Proof, err error) {
 }
 
 // Check the proof of innocent, it is called from precompiled contracts of EVM package.
-func CheckProof(packedProof []byte) error {
+func CheckProof(packedProof []byte, chain core.ChainContext) error {
 	innocentProof, err := UnpackProof(packedProof)
 	if err != nil {
 		return err
 	}
-	err = validateInnocentProof(innocentProof)
+	err = validateInnocentProof(innocentProof, chain)
 	if err != nil {
 		return err
 	}
@@ -37,13 +59,13 @@ func CheckProof(packedProof []byte) error {
 }
 
 // validate challenge, call from EVM package.
-func CheckChallenge(packedProof []byte) error {
+func CheckChallenge(packedProof []byte, chain core.ChainContext) error {
 	challenge, err := UnpackProof(packedProof)
 	if err != nil {
 		return err
 	}
 
-	err = validateChallenge(challenge)
+	err = validateChallenge(challenge, chain)
 	if err != nil {
 		return err
 	}
