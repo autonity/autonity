c28c56d0dd2abe4947f1aa40f938ee1ae8aaac11
Jason.Chen <jason.chen@clearmatics.com>
2021-01-26 13:03:44 +0000

    Subscribe chainevent in afd, add take challenge logics to provide proof of innocent.

--- afd/afd.go
+++ afd/afd.go
@@ -1,6 +1,8 @@
 package afd
 
 import (
+	"github.com/clearmatics/autonity/common"
+	"github.com/clearmatics/autonity/core"
 	"github.com/clearmatics/autonity/core/types"
 	"github.com/clearmatics/autonity/event"
 	"sync"
@@ -10,12 +12,60 @@ type FaultDetector struct {
 	wg sync.WaitGroup
 	afdFeed event.Feed
 	scope event.SubscriptionScope
+
+	blockChan chan core.ChainEvent
+	blockSub event.Subscription
+	blockchain *core.BlockChain
+	address common.Address
+}
+
+func NewFaultDetector(chain *core.BlockChain, nodeAddress common.Address) *FaultDetector {
+	fd := &FaultDetector{
+		address: nodeAddress,
+		blockChan:  make(chan core.ChainEvent, 300),
+		blockchain: chain,
+	}
+	return fd
+}
+
+// listen for external events like new block be committed on the chain, to get latest view of on-chain challenges, if
+// there are challenges rise to current validator, then take the challenge and provide the proof of innocent if possible.
+func (fd *FaultDetector) Run() {
+	fd.blockSub = fd.blockchain.SubscribeChainEvent(fd.blockChan)
+	for {
+		select {
+		case ev := <-fd.blockChan:
+			err := fd.takeMyChallenge(ev.Block, ev.Hash)
+			if err != nil {
+				// prints something.
+			}
+		}
+	}
+}
+
+// get challenges from blockchain via blockchain.autonityContract calls.
+func (fd *FaultDetector) takeMyChallenge(block *types.Block, hash common.Hash) error {
+	//todo: get challenges from blockchain via blockchain.autonityContract go wrappers.
+
+	//todo: get those challenges should take by myself.
+
+	//todo: get proof for each challenge
+
+	//todo: send proofs via SendOnChainProofs() function.
+	return nil
+}
+
+func (fd *FaultDetector) Stop() {
+	fd.scope.Close()
+	fd.blockSub.Unsubscribe()
+	fd.wg.Wait()
 }
 
 func (fd *FaultDetector) SubscribeAFDEvents(ch chan<- types.SubmitProofEvent) event.Subscription {
 	return fd.scope.Track(fd.afdFeed.Subscribe(ch))
 }
 
+// afd send proof to ethereum object which will submit the on-chain proofs via transaction.
 func (fd *FaultDetector) SendOnChainProofs(t types.ProofType,  proofs[]types.OnChainProof) {
 	fd.wg.Add(1)
 	go func() {
