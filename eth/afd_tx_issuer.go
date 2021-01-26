aac5dba1d7a9e45412e959a9c31292308ae948ce
Jason.Chen <jason.chen@clearmatics.com>
2021-01-26 11:40:41 +0000

    Move afd event processing logics into individual file, afd_tx_issuer.go

---
+++ eth/afd_tx_issuer.go
@@ -0,0 +1,26 @@
+package eth
+
+import "github.com/clearmatics/autonity/core/types"
+
+func (s *Ethereum) sendAccountabilityTransaction(e types.SubmitProofEvent) {
+	if e.Type == types.InnocentProof {
+		//todo: pack innocent proof transaction with abi and send it.
+	}
+
+	if e.Type == types.ChallengeProof {
+		//todo: pack challenge proof transaction with abi and send it.
+	}
+}
+
+func (s *Ethereum) afdEventLoop() {
+	for {
+		select {
+		case event := <-s.afdCh:
+			//todo: send accountability proofs.
+			s.sendAccountabilityTransaction(event)
+		// Err() channel will be closed when unsubscribing.
+		case <-s.afdSub.Err():
+			return
+		}
+	}
+}
