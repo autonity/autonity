37703554586d1ab12581eb2c0acafcf3c8f9afea
Jason.Chen <jason.chen@clearmatics.com>
2021-02-01 07:22:58 +0000

    Move accountablity contracts into afd package, remove initchaincontext interface.

--- core/types/afd_types.go
+++ core/types/afd_types.go
@@ -17,19 +17,17 @@ const (
 // Since precompiled contract take raw bytes as input, so it should be rlp encoded into bytes before client send the
 // proof into autonity contract.
 type RawProof struct {
-	ParentHash common.Hash // use by precompiled contract to get committee from chain db.
 	Rule       uint8       // rule id.
 	Message    []byte      // the raw rlp encoded msg to be considered as suspicious one
 	Evidence   [][]byte    // the raw rlp encoded msgs as proof of innocent or misbehavior.
 }
 
 func (p *RawProof) EncodeRLP(w io.Writer) error {
-	return rlp.Encode(w, []interface{}{p.ParentHash, p.Rule, p.Message, p.Evidence})
+	return rlp.Encode(w, []interface{}{p.Rule, p.Message, p.Evidence})
 }
 
 func (p *RawProof) DecodeRLP(s *rlp.Stream) error {
 	var proof struct {
-		ParentHash common.Hash
 		Rule uint8
 		Message []byte
 		Evidence [][]byte
@@ -38,13 +36,12 @@ func (p *RawProof) DecodeRLP(s *rlp.Stream) error {
 		return err
 	}
 
-	p.ParentHash, p.Rule, p.Message, p.Evidence = proof.ParentHash, proof.Rule, proof.Message, proof.Evidence
+	p.Rule, p.Message, p.Evidence = proof.Rule, proof.Message, proof.Evidence
 	return nil
 }
 
 // RawProof to be decode into Proof for processing.
 type Proof struct {
-	ParentHash common.Hash
 	Rule       uint8
 	Message    ConsensusMessage   // the msg to be considered as suspicious one
 	Evidence   []ConsensusMessage // the msgs as proof of innocent or misbehavior.
