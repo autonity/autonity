d17efec0cf7b6c87aef290ee8fe218f4afd8d64f
Jason.Chen <jason.chen@clearmatics.com>
2021-02-05 11:48:17 +0000

    Add basic interface for msg store.

--- afd/msg_store.go
+++ afd/msg_store.go
@@ -1,16 +1,24 @@
 package afd
 
 import (
+	"github.com/clearmatics/autonity/common"
 	"github.com/clearmatics/autonity/core/types"
-	"math/big"
 )
 
 type MsgStore struct {
-	//map[Height][Round][MsgType] []ConsensusMessage
-	messages map[*big.Int]map[uint64]map[uint64][]types.ConsensusMessage
+	//map[Height]map[Round]map[MsgType]map[common.address][]ConsensusMessage
+	/*
+	  height->round->MsgCode->MsgSender-> [msg, msg,,,]
+	 */
+	messages map[uint64]map[int64]map[uint64]map[common.Address][]types.ConsensusMessage
 }
 
 // store msg into msg store, it returns proofs of equivocation, and errEquivocation
 func(ms *MsgStore) StoreMsg(m *types.ConsensusMessage) ([]types.ConsensusMessage, error) {
 	return nil, nil
+}
+
+// clean those ancient msgs.
+func(ms *MsgStore) cleanAncientMsg(fromHeight uint64) {
+	//todo: clean up ancient msgs by block height.
 }
