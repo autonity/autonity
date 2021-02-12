3d19fa831890110586c6ed371c03759631f53417
Piers Powlesland <pierspowlesland@gmail.com>
2021-02-12 20:33:19 +0000

    Remove unused method

--- consensus/tendermint/oracle.go
+++ consensus/tendermint/oracle.go
@@ -49,14 +49,3 @@ func (o *oracle) Valid(value algorithm.ValueID) bool {
 func (o *oracle) Height() uint64 {
 	return o.height
 }
-
-func (o *oracle) Value() (algorithm.ValueID, error) {
-	v, err := o.ba.value(o.height)
-	if err != nil {
-		return [32]byte{}, err
-	}
-	// The tendermint is making a proposal, we need to ensure that we add the proposal block to the msg store, so that
-	// it can be picked up in buildMessage.
-	o.store.addValue(v.Hash(), v)
-	return algorithm.ValueID(v.Hash()), nil
-}
