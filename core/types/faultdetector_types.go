6a636d3a834df945813d1ae29e7f08a3953b7744
Aneeque Safdar <aneequesafdar@gmail.com>
2021-02-25 13:10:31 +0000

    fmt

--- core/types/faultdetector_types.go
+++ core/types/faultdetector_types.go
@@ -7,6 +7,7 @@ import (
 )
 
 type ProofType uint64
+
 const (
 	InnocentProof ProofType = iota
 	ChallengeProof
@@ -14,6 +15,7 @@ const (
 )
 
 type Rule uint8
+
 const (
 	PN Rule = iota
 	PO
@@ -22,10 +24,10 @@ const (
 	C
 	C1
 
-	GarbageMessage		// message was signed by valid member, but it cannot be decoded.
-	InvalidProposal     // The value proposed by proposer cannot pass the blockchain's validation.
-	InvalidProposer     // A proposal sent from none proposer nodes of the committee.
-	Equivocation        // Multiple distinguish votes(proposal, prevote, precommit) sent by validator.
+	GarbageMessage  // message was signed by valid member, but it cannot be decoded.
+	InvalidProposal // The value proposed by proposer cannot pass the blockchain's validation.
+	InvalidProposer // A proposal sent from none proposer nodes of the committee.
+	Equivocation    // Multiple distinguish votes(proposal, prevote, precommit) sent by validator.
 	UnknownRule
 )
 
@@ -33,9 +35,9 @@ const (
 // Since precompiled contract take raw bytes as input, so it should be rlp encoded into bytes before client send the
 // proof into autonity contract.
 type RawProof struct {
-	Rule       Rule       // rule id.
-	Message    []byte      // the raw rlp encoded msg to be considered as suspicious one
-	Evidence   [][]byte    // the raw rlp encoded msgs as proof of innocent or misbehavior.
+	Rule     Rule     // rule id.
+	Message  []byte   // the raw rlp encoded msg to be considered as suspicious one
+	Evidence [][]byte // the raw rlp encoded msgs as proof of innocent or misbehavior.
 }
 
 func (p *RawProof) EncodeRLP(w io.Writer) error {
@@ -44,8 +46,8 @@ func (p *RawProof) EncodeRLP(w io.Writer) error {
 
 func (p *RawProof) DecodeRLP(s *rlp.Stream) error {
 	var proof struct {
-		Rule Rule
-		Message []byte
+		Rule     Rule
+		Message  []byte
 		Evidence [][]byte
 	}
 	if err := s.Decode(&proof); err != nil {
@@ -58,9 +60,9 @@ func (p *RawProof) DecodeRLP(s *rlp.Stream) error {
 
 // Proof is what to prove that one is misbehaving, one should be slashed when a valid proof is rise.
 type Proof struct {
-	Rule       Rule
-	Message    ConsensusMessage   // the msg to be considered as suspicious one
-	Evidence   []ConsensusMessage // the msgs as proof of innocent or misbehavior.
+	Rule     Rule
+	Message  ConsensusMessage   // the msg to be considered as suspicious one
+	Evidence []ConsensusMessage // the msgs as proof of innocent or misbehavior.
 }
 
 // OnChainProof to be stored by autonity contract for on-chain proof management.
@@ -68,11 +70,11 @@ type OnChainProof struct {
 	Sender  common.Address `abi:"sender"`
 	Msghash common.Hash    `abi:"msghash"`
 	// rlp enoded bytes for struct Rawproof object.
-	Rawproof []byte        `abi:"rawproof"`
+	Rawproof []byte `abi:"rawproof"`
 }
 
 // event to submit proofs via standard transaction.
 type SubmitProofEvent struct {
 	Proofs []OnChainProof
-	Type ProofType
+	Type   ProofType
 }
