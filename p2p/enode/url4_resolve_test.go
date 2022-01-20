de5c3c1d4b82c23ca8cc7664a98e5e69255e76e8
yazzaoui <ya@clearmatics.com>
2022-01-19 20:42:09 -0500

    Fix enode tests

--- p2p/enode/url4_resolve_test.go
+++ p2p/enode/url4_resolve_test.go
@@ -35,7 +35,7 @@ func TestParseNodeWithDomainResolution(t *testing.T) {
 		// Complete nodes with IP address.
 		{
 			rawurl:    "enode://1dd9d65c4552b5eb43d5ad55a2ee3f56c6cbc1c64a5c8d659f51fcd51bace24351232b8d7821617d2b29b54b81cdefb9b3e9c37d7fd5f63270bcc9e1a6f6a439@hostname:3",
-			wantError: `lookup hostname: `,
+			wantError: `lookup hostname`,
 		},
 		{
 			rawurl:    "enode://1dd9d65c4552b5eb43d5ad55a2ee3f56c6cbc1c64a5c8d659f51fcd51bace24351232b8d7821617d2b29b54b81cdefb9b3e9c37d7fd5f63270bcc9e1a6f6a439@127.0.0.1:foo",
