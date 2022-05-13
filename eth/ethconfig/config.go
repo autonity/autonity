6f03193597ba08cad1bba5725893ec218d8439b6
yazzaoui <ya@clearmatics.com>
2022-05-13 08:28:09 -0600

    Bump default miner gas target to 30M

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -84,7 +84,7 @@ var Defaults = Config{
 	TrieTimeout:             60 * time.Minute,
 	SnapshotCache:           102,
 	Miner: miner.Config{
-		GasCeil:  8000000,
+		GasCeil:  30_000_000,
 		GasPrice: big.NewInt(params.GWei),
 		Recommit: 3 * time.Second,
 	},
