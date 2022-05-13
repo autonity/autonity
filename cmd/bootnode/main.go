36626f69e56bec26ad24b36c5c1a904e881d0094
Jason Chen <54585152+Jason-Zhangxin-Chen@users.noreply.github.com>
2022-05-13 08:25:57 +0100

    fix, bootnode binary crashed due to the duplicated flag.

--- cmd/bootnode/main.go
+++ cmd/bootnode/main.go
@@ -44,7 +44,8 @@ func main() {
 		netrestrict = flag.String("netrestrict", "", "restrict network communication to the given IP networks (CIDR masks)")
 		runv5       = flag.Bool("v5", false, "run a v5 topic discovery bootnode")
 		verbosity   = flag.Int("verbosity", int(log.LvlInfo), "log verbosity (0-5)")
-		vmodule     = flag.String("vmodule", "", "log verbosity pattern")
+		// "vmodule" is duplicated with internal/debug/flags.go, so use "vmodulebootnode" to fix the panic.
+		vmodule = flag.String("vmodulebootnode", "", "log verbosity pattern")
 
 		nodeKey *ecdsa.PrivateKey
 		err     error
