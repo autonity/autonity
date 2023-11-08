d5d2f3ea8e5f7a4eed0fba8660390157d0dabdfd
lorenzo <31852651+lorenzo-dev1@users.noreply.github.com>
2023-11-08 09:21:55 +0100

    segregated gossiping tests

---
+++ e2e_test/simulations/README.md
@@ -0,0 +1,2 @@
+This tests are supposed to be used to experiment with custom network topologies and gossiping logic, not to do actual correctness testing.
+Therefore they are segregated to their own package and not executed on the CI.
--- e2e_test/simulations/gossiper_test.go
+++ e2e_test/simulations/gossiper_test.go
@@ -1,4 +1,4 @@
-package e2e
+package simulations
 
 import (
 	"math"
@@ -9,6 +9,7 @@ import (
 	"github.com/autonity/autonity/consensus/tendermint/backend"
 	"github.com/autonity/autonity/consensus/tendermint/core/interfaces"
 	"github.com/autonity/autonity/core/types"
+	e2e "github.com/autonity/autonity/e2e_test"
 	"github.com/autonity/autonity/log"
 	"github.com/autonity/autonity/node"
 	lru "github.com/hashicorp/golang-lru"
@@ -90,14 +91,14 @@ func (cg *customGossiper) AskSync(_ *types.Header) {
 
 // this test just has the purpose of verifying that the customGossiper works as intended
 func TestCustomGossiper(t *testing.T) {
-	vals, err := Validators(t, 10, "10e18,v,100,0.0.0.0:%s,%s")
+	vals, err := e2e.Validators(t, 10, "10e18,v,100,0.0.0.0:%s,%s")
 	require.NoError(t, err)
 
 	for _, val := range vals {
 		val.TendermintServices = &node.TendermintServices{Gossiper: newCustomGossiper}
 	}
 	// creates a network of 6 vals and starts all the nodes in it
-	network, err := NewNetworkFromValidators(t, vals, true)
+	network, err := e2e.NewNetworkFromValidators(t, vals, true)
 	require.NoError(t, err)
 	defer network.Shutdown()
 
