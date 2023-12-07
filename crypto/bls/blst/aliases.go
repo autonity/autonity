ad62e36b34d8c1cba1390875d1ebf1c662c1f2c3
Jason Chen <54585152+Jason-Zhangxin-Chen@users.noreply.github.com>
2023-12-07 04:01:42 +0000

    doc, add comments of internal types for blst.
    Co-authored-by: lorenzo <31852651+lorenzo-dev1@users.noreply.github.com>

--- crypto/bls/blst/aliases.go
+++ crypto/bls/blst/aliases.go
@@ -2,7 +2,9 @@ package blst
 
 import bind "github.com/supranational/blst/bindings/go"
 
-// Internal types for blst.
+// Internal types for blst. They determine which group is used for public keys and which for signatures.
+// Currently we use the small-and-fast group for public keys and the slow-and-big group for signatures.
+// for more context checkout: https://medium.com/nethermind-eth/bls-signatures-withdrawals-bbf38658c242#eea8
 type blstPublicKey = bind.P1Affine
 type blstSignature = bind.P2Affine
 type blstAggregatePublicKey = bind.P1Aggregate
