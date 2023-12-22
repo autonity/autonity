3ef0ee1dc1e3db696a13df1f62b6949f5f53a139
Jason Chen <jason.chen@clearmatics.com>
2023-12-22 07:22:11 +0000

    improvement, POP gen backward compatibility with legacy node key file..

--- cmd/autonity/misccmd_test.go
+++ cmd/autonity/misccmd_test.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"fmt"
+	"github.com/autonity/autonity/common/hexutil"
 	"github.com/autonity/autonity/crypto"
+	"github.com/stretchr/testify/require"
 	"io/ioutil"
 	"path/filepath"
 	"testing"
+	"time"
 )
 
 func TestGenPOP(t *testing.T) {
 
 	tests := []struct {
-		name, nodeKey, oracleKey, treasury, output string
-		useNodeKeyFile, useOracleKeyFile           bool
+		name, nodeKey, oracleKey, treasury, output      string
+		useNodeKeyFile, useOracleKeyFile, legacyNodeKey bool
 	}{
+		{
+			name:             "use correct legacy node key file to generate POP",
+			nodeKey:          "f1ab65d8d07ab6a7a2ab8419fa5bbaf8938f45556387d43f3f15967bc599a579",
+			oracleKey:        "198227888008a50b57bfb4d70ef5c4a3ef085538b148842fe3628b9005d66301",
+			treasury:         "0x850c1eb8d190e05845ad7f84ac95a318c8aab07f",
+			useNodeKeyFile:   true,
+			useOracleKeyFile: true,
+			legacyNodeKey:    true,
+		},
 		{
 			name:             "incorrect node key file",
 			nodeKey:          "f1ab65d8d07ab6a7a2ab8419fa5bbaf8938f45556387d43f3f15967bc599a5793cca398a63081b656790184794b9997073620e5d862750fd61b6e7fec3399ce3@",
@@ -82,12 +94,12 @@ func TestGenPOP(t *testing.T) {
 		test := test
 		t.Run(test.name, func(t *testing.T) {
 			t.Parallel()
-			genPOPWithExpect(t, test.nodeKey, test.oracleKey, test.treasury, test.output, test.useNodeKeyFile, test.useOracleKeyFile)
+			genPOPWithExpect(t, test.nodeKey, test.oracleKey, test.treasury, test.output, test.useNodeKeyFile, test.useOracleKeyFile, test.legacyNodeKey)
 		})
 	}
 }
 
-func genPOPWithExpect(t *testing.T, nodeKey, oracleKey, treasury, expected string, useNodeKeyFile, useOracleKeyFile bool) {
+func genPOPWithExpect(t *testing.T, nodeKey, oracleKey, treasury, expected string, useNodeKeyFile, useOracleKeyFile, legacyNodeKey bool) {
 	dir := tmpdir(t)
 	var geth *testautonity
 	if useNodeKeyFile && useOracleKeyFile {
@@ -116,7 +128,22 @@ func genPOPWithExpect(t *testing.T, nodeKey, oracleKey, treasury, expected strin
 		geth = runAutonity(t, "genOwnershipProof", "--nodekey", nodeKeyfile, "--oraclekeyhex", oracleKey, treasury)
 	}
 	defer geth.ExpectExit()
-	geth.Expect(expected)
+	if !legacyNodeKey {
+		geth.Expect(expected)
+		return
+	}
+
+	// wait for the child process to flush the new generated key to FS.
+	time.Sleep(time.Second * 2)
+	// construct expected string from generated consensus key.
+	nKey, cKey, err := crypto.LoadNodeKey(filepath.Join(dir, "nodekey.prv"))
+	require.NoError(t, err)
+	oKey, err := crypto.LoadECDSA(filepath.Join(dir, "oraclekey.prv"))
+	require.NoError(t, err)
+	pop, err := crypto.AutonityPOPProof(nKey, oKey, treasury, cKey)
+	require.NoError(t, err)
+	expectedOutPut := fmt.Sprintf("Node consensus key: %s\nSignatures: %s\n", cKey.PublicKey().Hex(), hexutil.Encode(pop))
+	geth.Expect(expectedOutPut)
 }
 
 func TestGenNodeKey(t *testing.T) {
