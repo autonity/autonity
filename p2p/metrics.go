5d76092062d6e15bfdba9f2af9fd8066e8db5b9e
Piyush Vijay <piyush.vijay@clearmatics.com>
2024-01-29 14:19:18 +0100

    fix lint issue

--- p2p/metrics.go
+++ p2p/metrics.go
@@ -56,7 +56,7 @@ type meteredConn struct {
 // newMeteredConn creates a new metered connection, bumps the ingress or egress
 // connection meter and also increases the metered peer count. If the metrics
 // system is disabled, function returns the original connection.
-func newMeteredConn(conn net.Conn, ingress bool, addr *net.TCPAddr, net Network) net.Conn {
+func newMeteredConn(conn net.Conn, ingress bool, _ *net.TCPAddr, net Network) net.Conn {
 	// Short circuit if metrics are disabled
 	if !metrics.Enabled {
 		return conn
