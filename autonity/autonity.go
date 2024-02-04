82d5e6d05849013c07423a2174831382fafe13f1
Piyush Vijay <piyush.vijay@clearmatics.com>
2024-02-04 13:24:44 +0100

    review comment fixes

--- autonity/autonity.go
+++ autonity/autonity.go
@@ -186,8 +186,8 @@ func (c *Cache) MinimumBaseFee() *big.Int {
 	return new(big.Int).Set(c.minBaseFee.Load())
 }
 
-func (c *AutonityContract) CommitteeEnodes(block *types.Block, db *state.StateDB, asATC bool) (*types.Nodes, error) {
-	return c.callGetCommitteeEnodes(db, block.Header(), asATC)
+func (c *AutonityContract) CommitteeEnodes(block *types.Block, db *state.StateDB, asACN bool) (*types.Nodes, error) {
+	return c.callGetCommitteeEnodes(db, block.Header(), asACN)
 }
 
 func (c *AutonityContract) MinimumBaseFee(block *types.Header, db vm.StateDB) (*big.Int, error) {
