eb2d776a98c24124f9437b74ff70cb067a0f1a4f
Jason Chen <jason.chen@clearmatics.com>
2024-04-03 12:05:44 +0100

    test, register validator with wrong inputs to close #653.

--- e2e_test/contracts/validator_mag_test.go
+++ e2e_test/contracts/validator_mag_test.go
@@ -3,6 +3,7 @@ package contracts
 import (
 	"crypto/ecdsa"
 	"github.com/autonity/autonity/crypto"
+	"github.com/autonity/autonity/crypto/blst"
 	e2e "github.com/autonity/autonity/e2e_test"
 	"github.com/autonity/autonity/p2p/enode"
 	"github.com/stretchr/testify/require"
@@ -38,6 +39,34 @@ func TestValidatorManagement(t *testing.T) {
 	timeout := 5 * time.Second
 	oracleAddr := crypto.PubkeyToAddress(oracle.PublicKey)
 	enodeURL := enode.V4DNSUrl(validator.PublicKey, "127.0.0.1", 30303, 30303) + ":30303"
+
+	// register validator with invalid enode should be failed.
+	err = operatorNode.AwaitRegisterValidator(validator, "", oracleAddr, consensusKey.PublicKey().Marshal(), pop, timeout)
+	require.NotNil(t, err)
+
+	// register validator with wrong TXN signer should be failed.
+	wrongSigner := delegator
+	err = operatorNode.AwaitRegisterValidator(wrongSigner, enodeURL, oracleAddr, consensusKey.PublicKey().Marshal(), pop, timeout)
+	require.NotNil(t, err)
+
+	// register validator with wrong oracle address should be failed.
+	wrongOracleAddress := crypto.PubkeyToAddress(delegator.PublicKey)
+	err = operatorNode.AwaitRegisterValidator(validator, enodeURL, wrongOracleAddress, consensusKey.PublicKey().Marshal(), pop, timeout)
+	require.NotNil(t, err)
+
+	// register validator with wrong consensus pub key should be failed.
+	wrongConsensusKey, err := blst.RandKey()
+	require.NoError(t, err)
+	err = operatorNode.AwaitRegisterValidator(validator, enodeURL, wrongOracleAddress, wrongConsensusKey.PublicKey().Marshal(), pop, timeout)
+	require.NotNil(t, err)
+
+	// register validator with wrong pop should be failed.
+	wrongPoP, err := crypto.AutonityPOPProof(validator, oracle, treasuryHex, wrongConsensusKey)
+	require.NoError(t, err)
+	err = operatorNode.AwaitRegisterValidator(validator, enodeURL, wrongOracleAddress, wrongConsensusKey.PublicKey().Marshal(), wrongPoP, timeout)
+	require.NotNil(t, err)
+
+	// register validator with valid meta data.
 	err = operatorNode.AwaitRegisterValidator(validator, enodeURL, oracleAddr, consensusKey.PublicKey().Marshal(), pop, timeout)
 	require.NoError(t, err)
 
