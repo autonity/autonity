bfd449fca09946365b25a6f67d4381b9a1a4c9a6
Piyush Vijay <piyush.vijay@clearmatics.com>
2024-05-31 10:59:15 +0200

    fix peer suspension

--- p2p/server.go
+++ p2p/server.go
@@ -1067,16 +1067,16 @@ func (srv *Server) checkInboundConn(remoteIP net.IP) error {
 	return nil
 }
 
-// TODO(lorenzo) can be removed once we implement it more cleanly in `processPeerSuspension`
-// right now the p2p server doesn't know about the epoch period, that is why this work around
-func (srv *Server) AddSuspension(id string, epochPeriod uint64) {
-	srv.suspended.add(id, srv.currentBlock.Load()+epochPeriod)
-}
-
 func (srv *Server) processPeerSuspension(pd peerDrop) {
-	reason := discReasonForError(pd.err)
-	if errors.Is(reason, DiscProtocolError) || reason == DiscSubprotocolError {
-		srv.suspended.add(pd.ID().String(), srv.currentBlock.Load()+protoErrorSuspensionSpan)
+	var p2pError *peerError
+	var protoError *ProtocolError
+	if errors.As(pd.err, &protoError) {
+		srv.suspended.add(pd.ID().String(), srv.currentBlock.Load()+protoError.Suspension())
+	} else if errors.As(pd.err, &p2pError) {
+		switch p2pError.code {
+		case errInvalidMsgCode, errInvalidMsg:
+			srv.suspended.add(pd.ID().String(), srv.currentBlock.Load()+p2pErrorSuspensionSpan)
+		}
 	}
 }
 
