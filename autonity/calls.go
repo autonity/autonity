a09e8d991a939aa5073fac96594f109bf8238993
Youssef Azzaoui <youssef.azzaoui@clearmatics.com>
2024-06-04 20:36:32 -0500

    Deterministic genesis delegations

--- autonity/calls.go
+++ autonity/calls.go
@@ -18,11 +18,15 @@ type raw []byte
 
 // GenesisBonds is an intermediary struct used to pass genesis bondings.
 // We cannot use autonity/core package here as it would cause import cycle
-type GenesisBonds = map[common.Address]GenesisBond
-
+type GenesisBonds = []GenesisBond
+type Delegation = struct {
+	Validator common.Address
+	Amount    *big.Int
+}
 type GenesisBond struct {
+	Staker        common.Address
 	NewtonBalance *big.Int
-	Bonds         map[common.Address]*big.Int
+	Bonds         []Delegation
 }
 
 func DeployContracts(genesisConfig *params.ChainConfig, genesisBonds GenesisBonds, evmContracts *GenesisEVMContracts) error {
@@ -299,53 +303,42 @@ func DeployAutonityContract(genesisConfig *params.AutonityContractGenesis, genes
 		},
 		ContractVersion: big.NewInt(1),
 	}
-
 	validators := make([]params.Validator, 0, len(genesisConfig.Validators))
 	for _, v := range genesisConfig.Validators {
 		validators = append(validators, *v)
 	}
-
-	err := evmContracts.DeployAutonityContract(genesisConfig.Bytecode, validators, contractConfig)
-	if err != nil {
+	if err := evmContracts.DeployAutonityContract(genesisConfig.Bytecode, validators, contractConfig); err != nil {
 		log.Error("DeployAutonityContract failed", "err", err)
 		return fmt.Errorf("failed to deploy Autonity contract: %w", err)
 	}
 
-	for addr, account := range genesisBonds {
-
+	for _, alloc := range genesisBonds {
 		balanceToMint := new(big.Int)
-
-		if account.NewtonBalance != nil {
-			balanceToMint.Add(balanceToMint, account.NewtonBalance)
+		if alloc.NewtonBalance != nil {
+			balanceToMint.Add(balanceToMint, alloc.NewtonBalance)
 		}
-
-		for _, amount := range account.Bonds {
-			balanceToMint.Add(balanceToMint, amount)
+		for _, delegation := range alloc.Bonds {
+			balanceToMint.Add(balanceToMint, delegation.Amount)
 		}
-
 		if balanceToMint.Cmp(common.Big0) > 0 {
-			err := evmContracts.Mint(addr, balanceToMint)
+			err := evmContracts.Mint(alloc.Staker, balanceToMint)
 			if err != nil {
 				return fmt.Errorf("error while minting Newton: %w", err)
 			}
-
-			for validatorAddress, amount := range account.Bonds {
-				err = evmContracts.Bond(addr, validatorAddress, amount)
+			for _, delegation := range alloc.Bonds {
+				err = evmContracts.Bond(alloc.Staker, delegation.Validator, delegation.Amount)
 				if err != nil {
 					return fmt.Errorf("error while bonding: %w", err)
 				}
 			}
 		}
-
 	}
 
-	err = evmContracts.FinalizeInitialization()
-	if err != nil {
+	if err := evmContracts.FinalizeInitialization(); err != nil {
 		return fmt.Errorf("error while calling finalizeInitialization: %w", err)
 	}
 
 	log.Info("Deployed Autonity contract", "address", params.AutonityContractAddress)
-
 	return nil
 }
 
