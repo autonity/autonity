1d4ba696ff8592a956ddc3fde4b456301f95e4a1
Youssef Azzaoui <youssef.azzaoui@clearmatics.com>
2024-06-18 08:28:28 -0500

    Parameters update v0.14.0

--- params/config.go
+++ params/config.go
@@ -68,9 +68,10 @@ var (
 	Ntn10000     = new(big.Int).Mul(big.NewInt(10_000), NtnPrecision)
 	Ntn40000     = new(big.Int).Mul(big.NewInt(40_000), NtnPrecision)
 
-	PiccadillyGenesisTime, _       = time.Parse(time.RFC3339, "2024-06-13T14:30:00Z")
+	PiccadillyGenesisTime, _       = time.Parse(time.RFC3339, "2024-06-21:14:00:00Z")
 	PiccadillyGenesisUnixTimestamp = PiccadillyGenesisTime.Unix()
-	// PiccadillyChainConfig contains the chain parameters to run a node on the Piccaddilly test network.
+
+	// PiccadillyChainConfig contains the chain parameters to run a node on the Piccadilly test network.
 	PiccadillyChainConfig = &ChainConfig{
 		ChainID:                 big.NewInt(65_100_003),
 		HomesteadBlock:          common.Big0,
