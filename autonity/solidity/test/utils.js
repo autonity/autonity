580245432bfccabbf88a721a1e1205ba4f6d79cd
Jason Chen <jason.chen@clearmatics.com>
2024-07-11 15:25:26 +0100

    ci, ci experience for truffle test.

--- autonity/solidity/test/utils.js
+++ autonity/solidity/test/utils.js
@@ -49,7 +49,16 @@ async function endEpoch(contract,operator,deployer){
   let delta = currentHeight - lastEpochBlock;
 
   let newEpochPeriod = delta + 5
-  await contract.setEpochPeriod(newEpochPeriod,{from: operator})
+  //await contract.setEpochPeriod(newEpochPeriod,{from: operator})
+  let txn = await contract.setEpochPeriod(newEpochPeriod, { from: operator });
+  // Wait for the transaction receipt
+  let receipt = await txn.wait();
+  // Check the status of the transaction
+  if (receipt.status === 1) {
+    console.log("Transaction setEpochPeriod successful!");
+  } else {
+    console.log("Transaction setEpochPeriod failed.");
+  }
 
   // close epoch
   console.log("currentHeight: ", currentHeight, "lastEpochBlock: ",
