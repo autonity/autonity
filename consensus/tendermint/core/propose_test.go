84b37d4aa8c03d822f729b35fb9f9cc7e0303f92
Jason Chen <jason.chen@clearmatics.com>
2024-08-12 08:21:24 +0100

    test, fix legacy tests of tendermint propose test.

--- consensus/tendermint/core/propose_test.go
+++ consensus/tendermint/core/propose_test.go
@@ -4,19 +4,19 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/autonity/autonity/common"
+	"github.com/autonity/autonity/consensus"
+	"github.com/autonity/autonity/consensus/tendermint/core/constants"
+	"github.com/autonity/autonity/consensus/tendermint/core/message"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
 	"math/big"
 	"reflect"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
-	"go.uber.org/mock/gomock"
-
-	"github.com/autonity/autonity/consensus"
 	"github.com/autonity/autonity/consensus/tendermint/core/committee"
-	"github.com/autonity/autonity/consensus/tendermint/core/constants"
 	"github.com/autonity/autonity/consensus/tendermint/core/interfaces"
-	"github.com/autonity/autonity/consensus/tendermint/core/message"
 	"github.com/autonity/autonity/core/types"
 	"github.com/autonity/autonity/crypto"
 	"github.com/autonity/autonity/crypto/blst"
@@ -34,9 +34,7 @@ func TestSendPropose(t *testing.T) {
 		proposerConsensusKey, err := blst.RandKey()
 		require.NoError(t, err)
 
-		messages := message.NewMap()
 		height := new(big.Int).SetUint64(1)
-		curRoundMessages := messages.GetOrCreate(1)
 		validRound := int64(1)
 		logger := log.New("backend", "test", "id", 0)
 
@@ -61,19 +59,18 @@ func TestSendPropose(t *testing.T) {
 		backendMock.EXPECT().SetProposedBlockHash(proposal.Block().Hash())
 		backendMock.EXPECT().Sign(gomock.Any()).AnyTimes().DoAndReturn(makeSigner(proposerConsensusKey))
 		backendMock.EXPECT().Broadcast(gomock.Any(), proposal)
-
+		roundState := newTendermintState(log.New(), nil, nil)
 		c := &Core{
-			address:          proposer,
-			backend:          backendMock,
-			curRoundMessages: curRoundMessages,
-			logger:           logger,
-			messages:         messages,
-			round:            1,
-			height:           big.NewInt(1),
-			validRound:       validRound,
-			committee:        valSet,
-			lastHeader:       &types.Header{Committee: testCommittee},
+			address:     proposer,
+			backend:     backendMock,
+			roundsState: roundState,
+			logger:      logger,
+			committee:   valSet,
+			lastHeader:  &types.Header{Committee: testCommittee},
 		}
+		c.SetHeight(common.Big1)
+		c.SetRound(1)
+		c.SetValidRoundAndValue(validRound, proposal.Block())
 
 		c.SetDefaultHandlers()
 		c.proposer.SendProposal(context.Background(), proposal.Block())
@@ -94,23 +91,22 @@ func TestHandleProposal(t *testing.T) {
 			Number: big.NewInt(1),
 		})
 
-		messages := message.NewMap()
-		curRoundMessages := messages.GetOrCreate(round)
 		proposal := message.NewPropose(round, height, 1, block, signer, signerMember)
 
 		ctrl := gomock.NewController(t)
 		backendMock := interfaces.NewMockBackend(ctrl)
 		backendMock.EXPECT().VerifyProposal(proposal.Block())
 		c := &Core{
-			address:          addr,
-			messages:         messages,
-			committee:        committeeSet,
-			curRoundMessages: curRoundMessages,
-			logger:           log.Root(),
-			round:            round,
-			height:           new(big.Int).SetUint64(height),
-			backend:          backendMock,
+			roundsState: newTendermintState(log.New(), nil, nil),
+			address:     addr,
+			committee:   committeeSet,
+			logger:      log.Root(),
+			backend:     backendMock,
 		}
+		c.SetHeight(new(big.Int).SetUint64(height))
+		c.SetRound(round)
+		c.roundsState.GetOrCreate(round)
+
 		c.SetDefaultHandlers()
 		err := c.proposer.HandleProposal(context.Background(), proposal)
 		require.NoError(t, err)
@@ -124,18 +120,17 @@ func TestHandleProposal(t *testing.T) {
 		block := types.NewBlockWithHeader(&types.Header{
 			Number: big.NewInt(1),
 		})
-		messages := message.NewMap()
-		curRoundMessages := messages.GetOrCreate(round + 1)
 		proposal := message.NewPropose(round, height, 1, block, signer, signerMember)
 		c := &Core{
-			address:          addr,
-			messages:         messages,
-			committee:        committeeSet,
-			curRoundMessages: curRoundMessages,
-			logger:           log.Root(),
-			round:            round + 1,
-			height:           new(big.Int).SetUint64(height),
+			address:     addr,
+			roundsState: newTendermintState(log.New(), nil, nil),
+			committee:   committeeSet,
+			logger:      log.Root(),
 		}
+		c.SetHeight(new(big.Int).SetUint64(height))
+		c.SetRound(round + 1)
+		c.roundsState.GetOrCreate(round + 1)
+
 		c.SetDefaultHandlers()
 		err := c.proposer.HandleProposal(context.Background(), proposal)
 		if !errors.Is(err, constants.ErrOldRoundMessage) {
@@ -143,11 +138,9 @@ func TestHandleProposal(t *testing.T) {
 		}
 	})
 	t.Run("msg from non-proposer given, error returned", func(t *testing.T) {
-		messages := message.NewMap()
 		block := types.NewBlockWithHeader(&types.Header{
 			Number: new(big.Int).SetUint64(height),
 		})
-		curRoundMessages := messages.GetOrCreate(2)
 
 		logger := log.New("backend", "test", "id", 0)
 
@@ -155,14 +148,14 @@ func TestHandleProposal(t *testing.T) {
 		proposal := message.NewPropose(round, height, 1, block, makeSigner(keys[nonProposer.Address].consensus), nonProposer)
 
 		c := &Core{
-			address:          addr,
-			messages:         messages,
-			curRoundMessages: curRoundMessages,
-			logger:           logger,
-			round:            round + 1,
-			height:           new(big.Int).SetUint64(height),
-			committee:        committeeSet,
+			roundsState: newTendermintState(log.New(), nil, nil),
+			address:     addr,
+			logger:      logger,
+			committee:   committeeSet,
 		}
+		c.SetHeight(new(big.Int).SetUint64(height))
+		c.SetRound(round + 1)
+		c.roundsState.GetOrCreate(2)
 
 		c.SetDefaultHandlers()
 		err := c.proposer.HandleProposal(context.Background(), proposal)
@@ -182,23 +175,22 @@ func TestHandleProposal(t *testing.T) {
 		block := types.NewBlockWithHeader(&types.Header{
 			Number: big.NewInt(1),
 		})
-		messageMap := message.NewMap()
-		curRoundMessages := messageMap.GetOrCreate(2)
 		proposal := newUnverifiedPropose(round, height, 1, block, signer, signerMember)
 		backendMock := interfaces.NewMockBackend(ctrl)
 		backendMock.EXPECT().Post(gomock.Any()).Times(0)
 
 		c := &Core{
-			address:          addr,
-			backend:          backendMock,
-			messages:         messageMap,
-			curRoundMessages: curRoundMessages,
-			logger:           log.Root(),
-			proposeTimeout:   NewTimeout(Propose, log.Root()),
-			committee:        committeeSet,
-			round:            round,
-			height:           new(big.Int).SetUint64(height),
+			roundsState:    newTendermintState(log.New(), nil, nil),
+			address:        addr,
+			backend:        backendMock,
+			logger:         log.Root(),
+			proposeTimeout: NewTimeout(Propose, log.Root()),
+			committee:      committeeSet,
 		}
+		c.SetHeight(new(big.Int).SetUint64(height))
+		c.SetRound(round)
+		c.roundsState.GetOrCreate(2)
+
 		c.SetDefaultHandlers()
 		err := c.proposer.HandleProposal(context.Background(), proposal)
 		fmt.Println(err)
@@ -210,8 +202,6 @@ func TestHandleProposal(t *testing.T) {
 		block := types.NewBlockWithHeader(&types.Header{
 			Number: new(big.Int).SetUint64(height),
 		})
-		messageMap := message.NewMap()
-		curRoundMessages := messageMap.GetOrCreate(round)
 		proposal := message.NewPropose(round, height, 1, block, signer, signerMember)
 		backendMock := interfaces.NewMockBackend(ctrl)
 		backendMock.EXPECT().VerifyProposal(gomock.Any()).Return(eventPostingDelay, consensus.ErrFutureTimestampBlock)
@@ -220,16 +210,16 @@ func TestHandleProposal(t *testing.T) {
 		}
 		backendMock.EXPECT().Post(event).Times(1)
 		c := &Core{
-			address:          addr,
-			backend:          backendMock,
-			messages:         messageMap,
-			curRoundMessages: curRoundMessages,
-			logger:           log.Root(),
-			proposeTimeout:   NewTimeout(Propose, log.Root()),
-			committee:        committeeSet,
-			round:            round,
-			height:           new(big.Int).SetUint64(height),
+			roundsState:    newTendermintState(log.New(), nil, nil),
+			address:        addr,
+			backend:        backendMock,
+			logger:         log.Root(),
+			proposeTimeout: NewTimeout(Propose, log.Root()),
+			committee:      committeeSet,
 		}
+		c.SetHeight(new(big.Int).SetUint64(height))
+		c.SetRound(round)
+		c.roundsState.GetOrCreate(round)
 
 		c.SetDefaultHandlers()
 		err := c.proposer.HandleProposal(context.Background(), proposal)
@@ -244,23 +234,21 @@ func TestHandleProposal(t *testing.T) {
 		ctrl := gomock.NewController(t)
 		block := types.NewBlockWithHeader(&types.Header{Number: big.NewInt(1)})
 
-		messages := message.NewMap()
-		curRoundMessages := messages.GetOrCreate(round)
 		proposal := message.NewPropose(round, height, 2, block, signer, signerMember)
 		backendMock := interfaces.NewMockBackend(ctrl)
 		backendMock.EXPECT().VerifyProposal(proposal.Block())
 
 		c := &Core{
-			address:          addr,
-			backend:          backendMock,
-			messages:         messages,
-			curRoundMessages: curRoundMessages,
-			logger:           log.Root(),
-			round:            round,
-			height:           big.NewInt(1),
-			proposeTimeout:   NewTimeout(Propose, log.Root()),
-			committee:        committeeSet,
+			address:        addr,
+			backend:        backendMock,
+			roundsState:    newTendermintState(log.New(), nil, nil),
+			logger:         log.Root(),
+			proposeTimeout: NewTimeout(Propose, log.Root()),
+			committee:      committeeSet,
 		}
+		c.SetHeight(common.Big1)
+		c.SetRound(round)
+		curRoundMessages := c.roundsState.GetOrCreate(round)
 
 		c.SetDefaultHandlers()
 		err := c.proposer.HandleProposal(context.Background(), proposal)
@@ -283,27 +271,23 @@ func TestHandleProposal(t *testing.T) {
 			Number: big.NewInt(1),
 		})
 
-		messages := message.NewMap()
-		curRoundMessages := messages.GetOrCreate(2)
-
 		proposal := message.NewPropose(2, 1, 2, proposalBlock, makeSigner(keys[proposer.Address].consensus), &proposer)
-
 		backendMock := interfaces.NewMockBackend(ctrl)
-
 		c := &Core{
 			address:          committeeSet.Committee()[0].Address,
 			backend:          backendMock,
-			messages:         messages,
-			curRoundMessages: curRoundMessages,
+			roundsState:      newTendermintState(log.New(), nil, nil),
 			logger:           logger,
-			round:            2,
-			height:           big.NewInt(1),
 			proposeTimeout:   NewTimeout(Propose, logger),
 			prevoteTimeout:   NewTimeout(Prevote, logger),
 			precommitTimeout: NewTimeout(Precommit, logger),
 			committee:        committeeSet,
-			step:             Precommit,
 		}
+		c.SetHeight(common.Big1)
+		c.SetRound(2)
+		c.UpdateStep(Precommit)
+		c.roundsState.GetOrCreate(2)
+
 		c.SetDefaultHandlers()
 		defer c.proposeTimeout.StopTimer()   // nolint: errcheck
 		defer c.precommitTimeout.StopTimer() // nolint: errcheck
@@ -331,8 +315,6 @@ func TestHandleProposal(t *testing.T) {
 			Number: big.NewInt(1),
 		})
 
-		messages := message.NewMap()
-		curRoundMessages := messages.GetOrCreate(round)
 		logger := log.New("backend", "test", "id", 0)
 		proposal := message.NewPropose(round, height, -1, block, signer, signerMember)
 		prevote := message.NewPrevote(round, height, block.Hash(), signer, signerMember, csize)
@@ -343,20 +325,18 @@ func TestHandleProposal(t *testing.T) {
 		c := &Core{
 			address:          addr,
 			backend:          backendMock,
-			messages:         messages,
-			curRoundMessages: curRoundMessages,
-			round:            round,
-			height:           big.NewInt(1),
-			lockedValue:      types.NewBlockWithHeader(&types.Header{}),
-			lockedRound:      -1,
+			roundsState:      newTendermintState(log.New(), nil, nil),
 			logger:           logger,
 			proposeTimeout:   NewTimeout(Propose, logger),
 			prevoteTimeout:   NewTimeout(Prevote, logger),
 			precommitTimeout: NewTimeout(Precommit, logger),
-			validRound:       -1,
 			committee:        committeeSet,
 			lastHeader:       &types.Header{Committee: committeeSet.Committee()},
 		}
+		c.SetHeight(common.Big1)
+		curRoundMessages := c.roundsState.GetOrCreate(round)
+		c.SetRound(round)
+		//c.SetLockedRoundAndValue(-1, types.NewBlockWithHeader(&types.Header{}))
 
 		c.SetDefaultHandlers()
 		err := c.proposer.HandleProposal(context.Background(), proposal)
@@ -371,16 +351,8 @@ func TestHandleProposal(t *testing.T) {
 	t.Run("valid proposal given, vr < curR with quorum, pre-vote is sent", func(t *testing.T) {
 		ctrl := gomock.NewController(t)
 		block := types.NewBlockWithHeader(&types.Header{Number: big.NewInt(int64(height))})
-		messages := message.NewMap()
-		curRoundMessage := messages.GetOrCreate(round)
 
 		proposal := message.NewPropose(round, height, round-1, block, signer, signerMember)
-
-		for i := 0; i < 3; i++ {
-			val, _ := committeeSet.GetByIndex(i)
-			prevote := message.NewPrevote(round-1, height, proposal.Block().Hash(), makeSigner(keys[val.Address].consensus), &val, csize)
-			messages.GetOrCreate(round - 1).AddPrevote(prevote)
-		}
 
 		backendMock := interfaces.NewMockBackend(ctrl)
 		backendMock.EXPECT().VerifyProposal(proposal.Block())
@@ -390,21 +362,23 @@ func TestHandleProposal(t *testing.T) {
 		c := &Core{
 			address:          addr,
 			backend:          backendMock,
-			curRoundMessages: curRoundMessage,
-			messages:         messages,
-			lockedRound:      -1,
-			round:            round,
-			height:           new(big.Int).SetUint64(height),
-			lockedValue:      nil,
+			roundsState:      newTendermintState(log.New(), nil, nil),
 			logger:           log.Root(),
 			proposeTimeout:   NewTimeout(Propose, log.Root()),
 			prevoteTimeout:   NewTimeout(Prevote, log.Root()),
 			precommitTimeout: NewTimeout(Precommit, log.Root()),
-			validRound:       0,
 			committee:        committeeSet,
 			lastHeader:       &types.Header{Committee: committeeSet.Committee()},
 		}
-
+		c.SetHeight(new(big.Int).SetUint64(height))
+		c.SetRound(round)
+		c.SetValidRoundAndValue(0, nil)
+		for i := 0; i < 3; i++ {
+			val, _ := committeeSet.GetByIndex(i)
+			prevote := message.NewPrevote(round-1, height, proposal.Block().Hash(), makeSigner(keys[val.Address].consensus), &val, csize)
+			c.roundsState.GetOrCreate(round - 1).AddPrevote(prevote)
+		}
+		curRoundMessage := c.roundsState.GetOrCreate(round)
 		c.SetDefaultHandlers()
 		err := c.proposer.HandleProposal(context.Background(), proposal)
 		if err != nil {
@@ -435,9 +409,10 @@ func TestHandleNewCandidateBlockMsg(t *testing.T) {
 
 		c := &Core{
 			logger:                 log.New("backend", "test", "id", 0),
-			height:                 big.NewInt(11),
+			roundsState:            newTendermintState(log.New(), nil, nil),
 			pendingCandidateBlocks: make(map[uint64]*types.Block),
 		}
+		c.SetHeight(big.NewInt(11))
 		c.SetDefaultHandlers()
 		c.proposer.HandleNewCandidateBlockMsg(context.Background(), oldHeightCandidate)
 		require.Equal(t, 0, len(c.pendingCandidateBlocks))
@@ -450,12 +425,10 @@ func TestHandleNewCandidateBlockMsg(t *testing.T) {
 		proposerKey := keys[proposer.Address].consensus
 		height := new(big.Int).SetUint64(1)
 
-		messages := message.NewMap()
 		preBlock := types.NewBlockWithHeader(&types.Header{
 			Number: big.NewInt(0),
 		})
 
-		curRoundMessages := messages.GetOrCreate(1)
 		validRound := int64(1)
 		logger := log.New("backend", "test", "id", 0)
 
@@ -470,15 +443,14 @@ func TestHandleNewCandidateBlockMsg(t *testing.T) {
 			pendingCandidateBlocks: make(map[uint64]*types.Block),
 			address:                proposer.Address,
 			backend:                backendMock,
-			curRoundMessages:       curRoundMessages,
+			roundsState:            newTendermintState(log.New(), nil, nil),
 			logger:                 logger,
-			messages:               messages,
-			round:                  1,
-			height:                 big.NewInt(1),
-			validRound:             validRound,
 			committee:              committeeSet,
 			lastHeader:             &types.Header{Committee: committeeSet.Committee()},
 		}
+		c.SetHeight(common.Big1)
+		c.SetRound(1)
+		c.SetValidRoundAndValue(validRound, nil)
 		c.SetDefaultHandlers()
 		c.pendingCandidateBlocks[uint64(0)] = preBlock
 		c.proposer.HandleNewCandidateBlockMsg(context.Background(), proposal.Block())
