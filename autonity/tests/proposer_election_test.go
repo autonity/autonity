1f760617c9628ef43cad8e9a76a4f5b36e3a7345
Jason Chen <jason.chen@clearmatics.com>
2024-08-13 11:52:07 +0100

    test, add todos for testing.

---
+++ autonity/tests/proposer_election_test.go
@@ -0,0 +1,4 @@
+package tests
+
+// todo: Jason, add the proposer election test here, to make sure that the proposer elected from the AC contract has the
+//  same deterministic result computed by the Epoch type.
