b59d2309a561f69bf6b88c7d23776462288df4bf
tbssajal <sajalbuet15@gmail.com>
2024-09-16 01:09:52 +0600

    fix tests

--- e2e_test/byzantine/collusion/helpers.go
+++ e2e_test/byzantine/collusion/helpers.go
@@ -122,7 +122,11 @@ func validProposer(address common.Address, h uint64, r int64, contract *autonity
 func sendPrevote(c *core.Core, rule autonity.Rule) {
 	h, r, v := getCollusion(rule).context()
 	// if the leader haven't set up the context, skip.
-	if v == nil || c.Height().Uint64() < h {
+	if v == nil {
+		return
+	}
+
+	if c.Height().Uint64() != h {
 		return
 	}
 
@@ -157,6 +161,11 @@ func sendProposal(c faultyBroadcaster, rule autonity.Rule, msg message.Msg) {
 	}
 
 	h, r, v := ctx.context()
+	if h < c.Height().Uint64() {
+		c.SetupCollusionContext()
+		c.BroadcastAll(msg)
+		return
+	}
 	if h != c.Height().Uint64() {
 		c.BroadcastAll(msg)
 		return
