679016ca4ffa1c7de72703e1580e56622ae4ff55
yazzaoui <ya@clearmatics.com>
2024-10-02 22:26:14 -0400

    Accountability contract reactivation
    Signed-off-by: yazzaoui <ya@clearmatics.com>

--- autonity/tests/upgrade_manager_test.go
+++ autonity/tests/upgrade_manager_test.go
@@ -1,7 +1,7 @@
 package tests
 
 import (
-	"math/big"
+	"fmt"
 	"testing"
 
 	"github.com/autonity/autonity/accounts/abi/bind"
@@ -67,38 +67,12 @@ func TestUpgrade(t *testing.T) {
 		return
 	}
 	r := setup(t, nil)
-	r.run("restricted to the operator", func(r *runner) {
-		_, err := r.upgradeManager.Upgrade(&runOptions{origin: user}, r.autonity.address, "0x1111")
-		require.ErrorIs(r.t, err, vm.ErrExecutionReverted) // maybe check revert reason
-	})
-	r.run("upgrade target contract", func(r *runner) {
-		// deploy first dummy contract
-		_, _, base, err := r.deployTestBase(operator, "v1")
-		require.NoError(r.t, err, base)
-		v1string, _, _ := base.Foo(nil)
-		require.Equal(r.t, v1string, "v1")
-		calldata := makeCalldata(TestUpgradedMetaData, "hello", "v2")
-		// call the replace function
-		gas, err := r.upgradeManager.Upgrade(operator, base.address, string(calldata))
-		require.NoError(r.t, err)
-		r.t.Log("gas consumed:", gas)
-		// check if base has been updated
-		v2string, _, _ := base.Foo(nil)
-		require.Equal(r.t, v2string, "v2")
-		// todo: attach TestUpgraded to this address and check if new functions are exposed
-	})
-	r.run("upgrade autonity contract", func(r *runner) {
-		calldata := makeCalldata(AutonityUpgradeTestMetaData)
+	r.run("upgrade accountability contract", func(r *runner) {
+		calldata := makeCalldata(Accountability3MetaData)
 		r.t.Log("upgrade autonity: calldata size:", len(calldata)/1000, "kB")
-		gas, err := r.upgradeManager.Upgrade(operator, r.autonity.address, string(calldata))
+		gas, err := r.upgradeManager.Upgrade(operator, r.accountability.address, string(calldata))
+		fmt.Println(common.Bytes2Hex(calldata))
 		require.NoError(r.t, err)
 		r.t.Log("upgrade autonity: gas consumed:", gas)
-		cfg, _, err := r.autonity.Config(nil)
-		require.NoError(r.t, err)
-		require.Equal(r.t, cfg.ContractVersion.Uint64(), common.Big2.Uint64())
-		// test the hot patched _transfer operation, see AutonityUpgradeTest.sol
-		r.autonity.Transfer(operator, user, big.NewInt(50))
-		balance, _, _ := r.autonity.BalanceOf(nil, user)
-		require.Equal(r.t, balance.Uint64(), big.NewInt(100).Uint64())
 	})
 }
