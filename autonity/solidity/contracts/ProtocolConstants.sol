72a6e098d4653d4194e05e7c14fb2911d5818311
Jason Chen <jason.chen@clearmatics.com>
2024-11-26 14:40:22 +0000

    doc, update comments.

--- autonity/solidity/contracts/ProtocolConstants.sol
+++ autonity/solidity/contracts/ProtocolConstants.sol
@@ -2,4 +2,5 @@
 pragma solidity ^0.8.19;
 
 uint256 constant SLASHING_RATE_PRECISION = 10_000; // 100% slashing rate.
-uint256 constant ORACLE_SLASHING_RATE_CAP = 1_000; // Capped with 10% slashing rate.
+// Todo: resolve a better cap for the protocol.
+uint256 constant ORACLE_SLASHING_RATE_CAP = 1_000; // Capped with 10% slashing rate for oracle accountability.
