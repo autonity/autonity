0f186c348b75d860e9b792aeb51f93f7b021f4a6
yazzaoui <ya@clearmatics.com>
2024-12-05 17:14:33 +0200

    Piccadilly Genesis allocs
    Signed-off-by: yazzaoui <ya@clearmatics.com>

--- params/config.go
+++ params/config.go
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
+//go:generate go run ../cmd/picgen
+
 package params
 
 import (
