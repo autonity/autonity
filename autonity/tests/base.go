c630e8c0457506807c40265e1ed30551d21b2239
Scott R. Malley <scott.r.malley@gmail.com>
2024-12-12 15:55:08 +0100

    fix tests broken by rebase

--- autonity/tests/base.go
+++ autonity/tests/base.go
@@ -541,6 +541,11 @@ func Setup(t *testing.T, configOverride func(*params.AutonityContractGenesis) *p
 	}
 	genesisConfig.Config.AutonityContractConfig = autonityGenesis
 
+	// ToDo: we should probably override this in the specific tests where it is needed
+	if genesisConfig.Config.StakeableVestingConfig.TotalNominal.Cmp(common.Big0) == 0 {
+		genesisConfig.Config.StakeableVestingConfig.TotalNominal = new(big.Int).Mul(big.NewInt(1_000_000), params.NTNDecimalFactor) // 1M NTN
+	}
+
 	//
 	// Step 1: Execute test genesis sequence
 	//
