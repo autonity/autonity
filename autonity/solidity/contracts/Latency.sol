f441fdec46853c9406efa7be2188400d4f2fa2fc
Jason Chen <jason.chen@clearmatics.com>
2025-01-15 09:14:02 +0000

    improvement, latency report rate limit for preventing unnecessary clustering reorg.

--- autonity/solidity/contracts/Latency.sol
+++ autonity/solidity/contracts/Latency.sol
@@ -10,22 +10,28 @@ contract Latency is AccessAutonity {
     mapping(address => mapping(address => uint8)) public latency;
     address[] public committee;
 
+    // to limit the unnecessary clustering reorg, we limit the report for 1 report per validator per epoch.
+    uint256 public roundID;
+    mapping(address => uint256) public reportedRound;
+
     constructor(address payable _autonity, address[] memory initialCommittee) AccessAutonity(_autonity) {
         committee = initialCommittee;
+        roundID = 1;
     }
 
-    // todo: a reported validator should not report again in an epoch to prevent the abusing of report which triggers
-    //  unnecessary heavy cluster reorg.
     function report(uint8[] memory _latency) external {
         require(_latency.length == committee.length, "Latency: invalid length");
+        require(reportedRound[msg.sender] != roundID, "Already reported at current epoch");
         for (uint256 i = 0; i < _latency.length; i++) {
             latency[msg.sender][committee[i]] = _latency[i];
         }
+        reportedRound[msg.sender] = roundID;
         emit Reported(msg.sender, _latency.length);
     }
 
     function setCommittee(address[] memory _committee) external onlyAutonity {
         committee = _committee;
+        roundID +=1;
     }
 
     function read() external view returns (uint8[][] memory) {
