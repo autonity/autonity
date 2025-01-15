7686c0e6c8f71df5ce7299c3577cc13487efa48b
Jason Chen <54585152+Jason-Zhangxin-Chen@users.noreply.github.com>
2025-01-15 11:48:34 +0800

    Distributed latency measurement (#1128)
    * assigned independent latency measurement window for each validator.
    
    * fix, a bug correction for fromLatencyMat in clustering.go
    
    * logging, refine logging for lattency reporter.
    
    * refine nammings.
    
    * doc, add todos for latency report rate limit.
    
    * test and refine, repair backend tests and add an improvement in lattency measurement.

--- consensus/tendermint/backend/backend_test.go
+++ consensus/tendermint/backend/backend_test.go
@@ -108,10 +108,13 @@ func TestAskSync(t *testing.T) {
 
 	broadcaster := consensus.NewMockBroadcaster(ctrl)
 	broadcaster.EXPECT().FindPeers(m).Return(peers)
+	rt := NewMockrouter(ctrl)
+	rt.EXPECT().SetBroadcaster(broadcaster)
+
 	b := &Backend{
 		database:      rawdb.NewMemoryDatabase(),
 		knownMessages: knownMessages,
-		gossiper:      NewGossiper(knownMessages, common.Address{}, log.New(), make(chan struct{})),
+		gossiper:      NewGossiper(knownMessages, common.Address{}, log.New(), make(chan struct{}), rt),
 		logger:        log.New("backend", "test", "id", 0),
 	}
 	b.SetBroadcaster(broadcaster)
@@ -145,11 +148,16 @@ func BenchmarkGossip(b *testing.B) {
 		mockedPeer.EXPECT().Cache().Return(addressCache).AnyTimes()
 	}
 
+	sender := common.Address{}
+	rt := NewMockrouter(ctrl)
+	rt.EXPECT().SetBroadcaster(broadcaster)
+	rt.EXPECT().Route(committee, gomock.Any(), sender).AnyTimes().Return(nil)
+
 	knownMessages := fixsizecache.New[common.Hash, bool](4997, 20, fixsizecache.HashKey[common.Hash])
 	bk := &Backend{
 		database:      rawdb.NewMemoryDatabase(),
 		knownMessages: knownMessages,
-		gossiper:      NewGossiper(knownMessages, common.Address{}, log.New(), make(chan struct{})),
+		gossiper:      NewGossiper(knownMessages, sender, log.New(), make(chan struct{}), rt),
 	}
 	bk.SetBroadcaster(broadcaster)
 
@@ -205,13 +213,17 @@ func TestGossip(t *testing.T) {
 	}
 
 	knownMessages := fixsizecache.New[common.Hash, bool](499, 10, fixsizecache.HashKey[common.Hash])
+	sender := common.Address{}
+	rt := NewMockrouter(ctrl)
+	rt.EXPECT().SetBroadcaster(broadcaster)
+	rt.EXPECT().Route(committee, gomock.Any(), sender).AnyTimes().Return(committee.Members)
+
 	b := &Backend{
 		database:      rawdb.NewMemoryDatabase(),
 		knownMessages: knownMessages,
-		gossiper:      NewGossiper(knownMessages, common.Address{}, log.New(), make(chan struct{})),
+		gossiper:      NewGossiper(knownMessages, common.Address{}, log.New(), make(chan struct{}), rt),
 	}
 	b.SetBroadcaster(broadcaster)
-
 	b.Gossip(committee, msg)
 	<-time.NewTimer(2 * time.Second).C
 	if c := atomic.LoadUint64(&counter); c != 4 {
