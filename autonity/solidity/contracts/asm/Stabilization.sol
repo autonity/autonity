8adeecf3243d92aa9e03222a8a991563490580db
Scott R. Malley <scott.r.malley@gmail.com>
2025-01-23 12:30:35 +0100

    fix liquidate interest payment forward to auctioneer

--- autonity/solidity/contracts/asm/Stabilization.sol
+++ autonity/solidity/contracts/asm/Stabilization.sol
@@ -319,7 +319,8 @@ contract Stabilization is IStabilization {
         ) revert NotLiquidatable();
 
         if (msg.value < debt) revert InsufficientPayment();
-        _supplyControl.burn{value: cdp.principal}();
+        _supplyControl.burn{value: debt - accrued}();
+        IAuctioneer(_auctioneer).paidInterest{value: accrued}();
 
         uint surplus = msg.value - debt;
 
@@ -506,7 +507,6 @@ contract Stabilization is IStabilization {
             _config.liquidationRatio
         );
         return borrowLimit > debtLimit ? debtLimit : borrowLimit;
-
     }
 
     /// Price the Collateral Token in Auton.
