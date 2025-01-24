dfe39dd04f0adadfeb490566353f82a6ac46a6a5
Jason Chen <jason.chen@clearmatics.com>
2025-01-24 11:44:38 +0000

    refine, remove duplicated contract configs.

--- autonity/genesis.go
+++ autonity/genesis.go
@@ -37,6 +37,18 @@ type (
 )
 
 var (
+	defaultContracts = AutonityContracts{
+		AccountabilityContract:         params.AccountabilityContractAddress,
+		OracleContract:                 params.OracleContractAddress,
+		AcuContract:                    params.ACUContractAddress,
+		SupplyControlContract:          params.SupplyControlContractAddress,
+		StabilizationContract:          params.StabilizationContractAddress,
+		UpgradeManagerContract:         params.UpgradeManagerContractAddress,
+		InflationControllerContract:    params.InflationControllerContractAddress,
+		OmissionAccountabilityContract: params.OmissionAccountabilityContractAddress,
+		LatencyContract:                params.LatencyContractAddress,
+	}
+
 	commonSequence = []genesisStep{
 		executeGenesisDelegations,
 		createAutonitySchedules,
@@ -154,17 +166,7 @@ func deployAutonityContract(config *params.ChainConfig, _ GenesisBonds, deploy g
 			WithheldRewardsPool:     config.AutonityContractConfig.WithheldRewardsPool,
 			TreasuryAccount:         config.AutonityContractConfig.Treasury,
 		},
-		Contracts: AutonityContracts{
-			AccountabilityContract:         params.AccountabilityContractAddress,
-			OracleContract:                 params.OracleContractAddress,
-			AcuContract:                    params.ACUContractAddress,
-			SupplyControlContract:          params.SupplyControlContractAddress,
-			StabilizationContract:          params.StabilizationContractAddress,
-			UpgradeManagerContract:         params.UpgradeManagerContractAddress,
-			InflationControllerContract:    params.InflationControllerContractAddress,
-			OmissionAccountabilityContract: params.OmissionAccountabilityContractAddress,
-			LatencyContract:                params.LatencyContractAddress,
-		},
+		Contracts: defaultContracts,
 		Protocol: AutonityProtocol{
 			OperatorAccount:     config.AutonityContractConfig.Operator,
 			EpochPeriod:         new(big.Int).SetUint64(config.AutonityContractConfig.EpochPeriod),
@@ -633,17 +635,7 @@ func deployAutonityTestContract(config *params.ChainConfig, _ GenesisBonds, depl
 			WithheldRewardsPool:     config.AutonityContractConfig.WithheldRewardsPool,
 			TreasuryAccount:         config.AutonityContractConfig.Treasury,
 		},
-		Contracts: AutonityContracts{
-			AccountabilityContract:         params.AccountabilityContractAddress,
-			OracleContract:                 params.OracleContractAddress,
-			AcuContract:                    params.ACUContractAddress,
-			SupplyControlContract:          params.SupplyControlContractAddress,
-			StabilizationContract:          params.StabilizationContractAddress,
-			UpgradeManagerContract:         params.UpgradeManagerContractAddress,
-			InflationControllerContract:    params.InflationControllerContractAddress,
-			OmissionAccountabilityContract: params.OmissionAccountabilityContractAddress,
-			LatencyContract:                params.LatencyContractAddress,
-		},
+		Contracts: defaultContracts,
 		Protocol: AutonityProtocol{
 			OperatorAccount:     config.AutonityContractConfig.Operator,
 			EpochPeriod:         new(big.Int).SetUint64(config.AutonityContractConfig.EpochPeriod),
