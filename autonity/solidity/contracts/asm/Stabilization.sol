00edab4587c695fab8b0a90237c2bbeb517bef36
Scott R. Malley <scott.r.malley@gmail.com>
2025-01-28 12:53:56 +0100

    fix max borrow calculation

--- autonity/solidity/contracts/asm/Stabilization.sol
+++ autonity/solidity/contracts/asm/Stabilization.sol
@@ -386,9 +386,9 @@ contract Stabilization is IStabilization {
     function setLiquidationRatio(
         uint256 ratio
     )
-    external
-    validRatios(ratio, _config.minCollateralizationRatio)
-    onlyOperator
+        external
+        validRatios(ratio, _config.minCollateralizationRatio)
+        onlyOperator
     {
         _config.liquidationRatio = ratio;
     }
