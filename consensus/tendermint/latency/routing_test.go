b8a4c3aad97d834c19d40533f9a84dc81e8bd0fc
Scott R. Malley <scott.r.malley@gmail.com>
2025-02-11 14:00:38 +0100

    fix some lint errors and reinstate cluster threshhold

--- consensus/tendermint/latency/routing_test.go
+++ consensus/tendermint/latency/routing_test.go
@@ -2,8 +2,23 @@ package latency
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+
+	"github.com/autonity/autonity/consensus"
+	"github.com/autonity/autonity/crypto"
 )
 
 func TestRouter(t *testing.T) {
 	// ToDo: test routing
+	t.Run("Test constructor should set self", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		broadcaster := consensus.NewMockBroadcaster(ctrl)
+		key, err := crypto.GenerateKey()
+		require.NoError(t, err)
+
+		router := NewRouter(broadcaster, key)
+		require.NotNil(t, router.self)
+	})
 }
