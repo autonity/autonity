ae0acf4e686d4888075f0801d526bba29b44171f
Jason Chen <jason.chen@clearmatics.com>
2025-03-31 15:03:38 +0100

    fix, start consensus engine once we have quorum connections.

--- eth/backend.go
+++ eth/backend.go
@@ -641,7 +641,7 @@ func (s *Ethereum) validatorController() {
 			for {
 				select {
 				case <-ticker.C:
-					if float64(s.consensusServer.PeerCount()) >= (float64(committee.Len()) * (2.0 / 3.0)) {
+					if float64(s.consensusServer.PeerCount()+1) >= (float64(committee.Len()) * (2.0 / 3.0)) {
 						mu.Lock()
 						if !wasValidating {
 							s.miner.Start()
