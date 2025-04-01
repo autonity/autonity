fd84d9c96bb1356f46440a308b5c318f65508a46
Scott R. Malley <scott.r.malley@gmail.com>
2025-04-01 14:55:11 +0200

    update truffle test solc version

--- autonity/solidity/truffle-config.js
+++ autonity/solidity/truffle-config.js
@@ -92,9 +92,9 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1", 
-      port: 8545,        
-      network_id: "*",   
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
       gas: 46123880
     },
   },
@@ -106,6 +106,7 @@ module.exports = {
   compilers: {
     solc: {
       version: "0.8.22",
+      evmVersion: "london",
       // docker: true,    
       optimizer: {
         enabled: true,
