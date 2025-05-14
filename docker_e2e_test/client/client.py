57618098e90a279c01c2062016017de32795ac20
Jason Chen <jason.chen@clearmatics.com>
2025-05-14 13:12:44 +0100

    test, ci experience for docker test.

--- docker_e2e_test/client/client.py
+++ docker_e2e_test/client/client.py
@@ -257,21 +257,25 @@ class Client(object):
             out.write(content)
 
     def cli_cmd(self):
-        cmd = "sudo {0} --genesis {1} --datadir {2} --autonitykeys {3} --syncmode 'full' --port {4} --consensus.port {5} " \
+        cmd = "sudo nohup {0} --genesis {1} --datadir {2} --autonitykeys {3} --syncmode 'full' --port {4} --consensus.port {5} " \
               "--http.port {6} --http --http.addr '0.0.0.0' --ws --ws.port {7} --http.corsdomain '*' " \
               "--http.api 'personal,debug,eth,net,web3,txpool,miner,tendermint' --networkid 1991 --allow-insecure-unlock " \
               "--graphql --unlock 0x{8} --password {9} --mine --miner.threads '1' " \
-              "--verbosity 4 --miner.gaslimit 10000000000 ".format(AUTONITY_PATH.format(self.ssh_user),
-                                                                    GENESIS_PATH.format(self.ssh_user),
-                                                                    CHAIN_DATA_DIR.format(self.ssh_user, self.host),
-                                                                    BOOT_KEY_FILE.format(self.ssh_user, self.host),
-                                                                    self.p2p_port,
-                                                                    self.acn_port,
-                                                                    self.rpc_port,
-                                                                    self.ws_port,
-                                                                    self.coin_base,
-                                                                    KEY_PASSPHRASE_FILE.format(self.ssh_user, self.host)
-                                                                    )
+              "--verbosity 4 --miner.gaslimit 10000000000 > {10} 2>&1 & ".format(AUTONITY_PATH.format(self.ssh_user),
+                                                                                 GENESIS_PATH.format(self.ssh_user),
+                                                                                 CHAIN_DATA_DIR.format(self.ssh_user,
+                                                                                                       self.host),
+                                                                                 BOOT_KEY_FILE.format(self.ssh_user,
+                                                                                                      self.host),
+                                                                                 self.p2p_port,
+                                                                                 self.acn_port,
+                                                                                 self.rpc_port,
+                                                                                 self.ws_port,
+                                                                                 self.coin_base,
+                                                                                 KEY_PASSPHRASE_FILE.format(
+                                                                                     self.ssh_user, self.host),
+                                                                                 LOG_PATH.format(self.ssh_user)
+                                                                                 )
         return cmd
 
     def generate_package(self):
@@ -340,11 +344,12 @@ class Client(object):
                     pattern=r'\[sudo\] password for ' + self.ssh_user + ':',
                     response=self.sudo_pass + '\n'
                 )
-                #cmd = SYSTEMD_START_CLIENT
+                # cmd = SYSTEMD_START_CLIENT
                 cmd = self.cli_cmd()
                 self.logger.info("*******Executing command: %s", cmd)
-                result = c.run(cmd, pty=True, watchers=[sudopass],
-                               warn=True, hide=False)
+                result = c.run("touch {}".format(LOG_PATH.format(self.ssh_user)), pty=True, watchers=[sudopass],
+                               warn=True, hide=True)
+                result = c.run(cmd, pty=True, watchers=[sudopass], warn=True, hide=True)
                 if result and result.exited == 0 and result.ok:
                     self.logger.info('system service started. %s', self.host)
                     self.client_stopped = False
