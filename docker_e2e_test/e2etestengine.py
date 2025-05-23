86e036b8165dc2948d4f664d89b0909e4e3f744b
Jason Chen <54585152+Jason-Zhangxin-Chen@users.noreply.github.com>
2025-05-23 19:14:31 +0800

    ci, install docker test dep with --fix-missing. (#1171)
    * test, install docker test dep with --fix-missing.
    
    * ci, ci experient for docker test flow.
    
    * ci, run docker batched tests to fix docker pull rate limit.
    
    * ci, rename ci job.

--- docker_e2e_test/e2etestengine.py
+++ docker_e2e_test/e2etestengine.py
@@ -5,7 +5,6 @@ import log
 from conf import conf
 from testcase.testcase import TestCase
 from planner.networkplanner import NetworkPlanner
-from client.client import Client
 import time
 
 LG = log.get_logger()
@@ -13,52 +12,65 @@ LG = log.get_logger()
 
 if __name__ == '__main__':
     LG.debug("##########################################")
-    LG.debug("")
-    LG.debug("")
-    LG.debug("Test Engine start.")
+    LG.debug("\n\nTest Engine start.")
 
     parser = argparse.ArgumentParser()
     parser.add_argument("autonity", help='Autonity Binary Path')
-    parser.add_argument("-id", help='testcase index', type=int, required=True, default=0)
+    parser.add_argument("-id", help='Starting testcase index', type=int, required=True, default=0)
 
     args = parser.parse_args()
     autonity_path = args.autonity
-    test_id = args.id
+    start_id = args.id
 
-    LG.debug(f"testcase index: {test_id}")
+    # Generate 3 consecutive test IDs
+    batch_size = 3
+    test_ids = range(start_id, start_id + batch_size)
+    LG.debug(f"Running tests: {list(test_ids)}")
 
     conf.load_project_conf()
-
-    # plan the autonity network configurations, and start all autonity clients.
     network_planner = NetworkPlanner(autonity_path)
     network_planner.plan()
-    network_planner.deploy()
+    network_planner.deploy_all_nodes()
     network_planner.start_all_nodes()
-
     clients = network_planner.get_clients()
-    try:
-        # load test case view, and start the specific test
-        test_set = conf.get_test_case_conf()
-        test_case = test_set["playbook"]["testcases"][test_id]
-        test = TestCase(test_case, clients)
-        LG.debug("")
-        LG.debug("")
-        LG.info("start test case: %s", test_case)
-        LG.debug("")
-        LG.debug("")
-        result = test.start_test()
-        if result is True:
-            LG.info("[TEST PASSED]")
-            exit(0)
-        if result is False:
-            LG.info("[TEST FAILED]")
-            LG.info("Log collecting for failed test ......")
-            time.sleep(180)
-            exit(1)
 
-    except (KeyError, TypeError) as e:
-        LG.error("Wrong configuration. %s", e)
-    except Exception as e:
-        LG.error("Get error: %s", e)
+    all_passed = True
+    test_set = conf.get_test_case_conf()
 
-    exit(1)
+    for test_id in test_ids:
+        try:
+            if test_id >= len(test_set["playbook"]["testcases"]):
+                break
+
+            LG.debug("\n" + "="*50)
+            LG.debug(f"Starting Test ID: {test_id}")
+
+            test_case = test_set["playbook"]["testcases"][test_id]
+            test = TestCase(test_case, clients)
+
+            LG.info("Starting test case: %s", test_case)
+            result = test.start_test()
+
+            if result:
+                LG.info(f"[TEST {test_id} PASSED]")
+            else:
+                LG.error(f"[TEST {test_id} FAILED]")
+                all_passed = False
+
+            network_planner.re_genesis_network()
+
+        except (KeyError, TypeError, IndexError) as e:
+            LG.error(f"Invalid test ID {test_id}: {e}")
+            all_passed = False
+        except Exception as e:
+            LG.error(f"Error in test ID {test_id}: {e}")
+            all_passed = False
+
+    if all_passed:
+        LG.info("[TEST PASSED]")
+        exit(0)
+    else:
+        LG.info("[TEST FAILED]")
+        LG.info("Log collecting for failed tests...")
+        time.sleep(180)
+        exit(1)
