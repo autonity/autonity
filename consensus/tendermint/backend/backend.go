5bdae69d400697aceccd9fc82e4d10bc97087eb1
Piyush Vijay <piyush.vijay@clearmatics.com>
2025-05-27 16:38:09 +0530

    minor refactoring

--- consensus/tendermint/backend/backend.go
+++ consensus/tendermint/backend/backend.go
@@ -98,7 +98,7 @@ func New(
 
 	peerFinder := backend.Broadcaster.(routerInterfaces.PeerFinder)
 
-	backend.router = router.SetupRouter(peerFinder, nodeKey, backend.address, pinger, selector, backend.logger)
+	backend.router = router.Setup(peerFinder, nodeKey, backend.address, pinger, selector, backend.logger)
 
 	backend.gossiper = NewGossiper(
 		backend.knownMessages,
