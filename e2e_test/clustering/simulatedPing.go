f97efae33f67908317644f12a56550464ae68314
Piyush Vijay <piyush.vijay@clearmatics.com>
2025-06-02 15:15:06 +0530

    end to end clustering test

--- e2e_test/clustering/simulatedPing.go
+++ e2e_test/clustering/simulatedPing.go
@@ -1,16 +1,22 @@
 package clustering
 
+import (
+	"context"
+	"math/rand"
+	"time"
+
+	"github.com/autonity/autonity/consensus/tendermint/router/ping"
+)
+
 // this type is used for local e2e testing.
-/*
 type simulatedPinger struct{}
 
 func NewSimulatedPinger() ping.Pinger {
 	return &simulatedPinger{}
 }
 
-func (*simulatedPinger) Ping(_ ping.Target, resultCh chan<- time.Duration) {
+func (*simulatedPinger) Ping(_ context.Context, target ping.Target) ping.Result {
 	// Generate random latency between 10ms and 500ms
 	latency := time.Duration(10+rand.Int63n(491)) * time.Millisecond
-	resultCh <- latency
+	return ping.Result{Latency: latency}
 }
-*/
