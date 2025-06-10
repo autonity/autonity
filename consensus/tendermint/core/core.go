1ff9049cabbb817e89b27185c121e394bb0707ee
Jason Chen <jason.chen@clearmatics.com>
2025-06-10 14:59:04 +0100

    revert, revert the changes of the triggering of ask-sync.

--- consensus/tendermint/core/core.go
+++ consensus/tendermint/core/core.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"math/big"
 	"sync"
-	"sync/atomic" //nolint
 	"time"
 
 	"github.com/autonity/autonity/autonity"
@@ -48,11 +47,7 @@ func New(backend interfaces.Backend, services *interfaces.Services, address comm
 		stepChange:             time.Now(),
 		noGossip:               noGossip,
 		eventCh:                make(chan events.CoreEvent, EventQueueSize),
-		syncState:              &SyncState{},
 	}
-	c.syncState.SetOutOfSync(false)             // initial state
-	c.syncState.SetLastValidMsgTime(time.Now()) // set initial timestamp
-	c.syncState.SetSyncTimeOut(syncTimeOut)     // e.g., syncTimeOut = 10*time.Second
 	c.SetDefaultHandlers()
 	if services != nil {
 		c.broadcaster = services.Broadcaster(c)
@@ -70,36 +65,6 @@ func (c *Core) SetDefaultHandlers() {
 	c.proposer = &Proposer{c}
 }
 
-type SyncState struct {
-	outOfSync        atomic.Bool
-	lastValidMsgTime atomic.Int64
-	timeOut          atomic.Int64
-}
-
-func (s *SyncState) SetOutOfSync(val bool) {
-	s.outOfSync.Store(val)
-}
-
-func (s *SyncState) IsOutOfSync() bool {
-	return s.outOfSync.Load()
-}
-
-func (s *SyncState) SetLastValidMsgTime(t time.Time) {
-	s.lastValidMsgTime.Store(t.UnixNano())
-}
-
-func (s *SyncState) GetLastValidMsgTime() time.Time {
-	return time.Unix(0, s.lastValidMsgTime.Load())
-}
-
-func (s *SyncState) SetSyncTimeOut(d time.Duration) {
-	s.timeOut.Store(int64(d))
-}
-
-func (s *SyncState) GetSyncTimeOut() time.Duration {
-	return time.Duration(s.timeOut.Load())
-}
-
 type Core struct {
 	blockPeriod uint64
 	address     common.Address
@@ -114,7 +79,6 @@ type Core struct {
 	timeoutEventSub     *event.TypeMuxSubscription
 	futureProposalTimer *time.Timer
 	stopped             chan struct{}
-	syncState           *SyncState
 
 	// map[Height]UnminedBlock
 	pendingCandidateBlocks map[uint64]*types.Block
@@ -402,16 +366,6 @@ func (c *Core) StartRound(ctx context.Context, round int64) {
 	}
 	c.processFuture(previousRound, round)
 	c.SendEvent(events.NewRoundChangeEvent(c.Height().Uint64(), round))
-}
-
-func (c *Core) updateSyncTimeout(timeout time.Duration) {
-	// if a round timer is greater than the current sync timeout, update the sync timeout
-	if timeout > c.syncState.GetSyncTimeOut() {
-		c.syncState.SetSyncTimeOut(timeout)
-	} else {
-		// otherwise reset to default
-		c.syncState.SetSyncTimeOut(syncTimeOut)
-	}
 }
 
 func (c *Core) setInitialState(r int64) {
--- consensus/tendermint/core/handler.go
+++ consensus/tendermint/core/handler.go
@@ -192,10 +192,6 @@ eventLoop:
 					}
 				}
 
-				// valid message, reset sync timeout
-				c.syncState.SetLastValidMsgTime(time.Now())
-				c.syncState.SetOutOfSync(false) // consider we are in sync, since we are receiving valid messages now
-
 				if !c.noGossip {
 					if !hadQuorum {
 						// if we did not have quorum and we reached it now
@@ -229,10 +225,6 @@ eventLoop:
 					c.logger.Debug("BacklogEvent message handling failed", "err", err)
 					continue
 				}
-
-				// valid message, reset sync timeout
-				c.syncState.SetLastValidMsgTime(time.Now())
-				c.syncState.SetOutOfSync(false) // consider we are in sync, since we are receiving valid messages now
 
 				if !c.noGossip {
 					if !hadQuorum {
@@ -299,29 +291,24 @@ func (c *Core) livenessTrackerLoop(ctx context.Context) {
 	syncMsg := c.createSyncMsg()
 	c.backend.AskSync(c.committee.Committee(), syncMsg)
 
+	ticker := time.NewTicker(time.Second * constants.AskSyncInterval)
+	defer ticker.Stop()
+
 eventLoop:
 	for {
 		select {
-		case <-time.After(time.Second * constants.AskSyncInterval): //check for sync every 5 seconds
-
-			if time.Since(c.syncState.GetLastValidMsgTime()) < c.syncState.GetSyncTimeOut() {
-				c.logger.Debug("Sync timeout not reached yet", "last valid message received", c.syncState.GetLastValidMsgTime(), "sync timeout", c.syncState.GetSyncTimeOut())
-				round = c.Round()
-				height = c.Height()
-				continue
-			}
+		case <-ticker.C:
 			currentRound := c.Round()
 			currentHeight := c.Height()
 
-			// we only ask for sync if the current view stayed the same for the past 30 seconds
 			if currentHeight.Cmp(height) == 0 && currentRound == round {
-				c.logger.Warn("⚠️ Consensus liveliness lost", "node", c.Address(), "height", height, "round", round, "step", c.Step())
+				c.logger.Warn("⚠️ Consensus liveliness lost")
 				syncMsg = c.createSyncMsg()
 				c.backend.AskSync(c.committee.Committee(), syncMsg)
-				c.syncState.SetOutOfSync(true)
 			}
 			round = currentRound
 			height = currentHeight
+
 		case <-ctx.Done():
 			c.logger.Debug("livenessTrackerLoop is stopped", "event", ctx.Err())
 			break eventLoop
