b191d129a4a879509a42303976712b323ec7a363
tbssajal <sajalbuet15@gmail.com>
2025-06-30 20:00:40 +0600

    vesting contracts from repo

--- autonity/solidity/contracts/interfaces/IStakeableVestingManager.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: LGPL-3.0-only
-pragma solidity >=0.8.2;
-
-interface IStakeableVestingManager {
-    function getStakeableVestingLogicContract() external view returns (address);
-}
