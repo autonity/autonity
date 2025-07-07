6f7d43127e79f578735b73bcc1e893746efa34b1
tbssajal <sajalbuet15@gmail.com>
2025-07-07 14:15:54 +0600

    tests

--- consensus/tendermint/core/message/helper.go
+++ consensus/tendermint/core/message/helper.go
@@ -38,3 +38,33 @@ func (e *EvidenceVote) ToPrevote() *Prevote {
 		},
 	}
 }
+
+func (p *Prevote) ToEvidence() *EvidenceVote {
+	signers := p.signers.ToQuorumSigners()
+	payload, _ := rlp.EncodeToBytes(extVote[uint32]{
+		Code:      EvidenceVoteCode,
+		Round:     uint64(p.round), // #nosec
+		Height:    p.height,
+		Value:     p.value,
+		Signers:   signers.SignersBase,
+		Signature: p.signature.(*blst.BlsSignature),
+	})
+
+	return &EvidenceVote{
+		value: p.value,
+		vote: vote[uint32]{
+			signers: signers.SignersBase,
+			base: base{
+				height:         p.height,
+				round:          p.round,
+				signatureInput: p.signatureInput,
+				signature:      p.signature,
+				payload:        payload,
+				hash:           crypto.Hash(payload),
+				verified:       p.verified,
+				preverified:    p.preverified,
+				signerKey:      p.signerKey,
+			},
+		},
+	}
+}
