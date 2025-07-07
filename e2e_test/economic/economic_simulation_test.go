60dc670a2ae23d8fda189d2d6ca9f34540390111
Jason Chen <jason.chen@clearmatics.com>
2025-07-07 13:23:07 +0100

    test, test refine.

--- e2e_test/economic/economic_simulation_test.go
+++ e2e_test/economic/economic_simulation_test.go
@@ -8,37 +8,40 @@ import (
 
 // TestDefaultConfig tests the economic model with different block-filling scenarios.
 func TestDefaultConfig(t *testing.T) {
+	//tenMins := uint64(600)
+	thirtyMins := uint64(1800)
 	tests := []struct {
 		name   string
 		blocks uint64
 		params *systemParams
 		packer TXNPacker
 	}{
-		// under block gas target tests
-		{"dust filled blocks", 3600 * 24, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(0.01, -2)}},
+		/*
+			// under block gas target tests
+			{"dust filled blocks", 3600 * 24, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(0.01, -2)}},
 
-		{"1/10 filled blocks", 3600, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(0.1, -2)}},
-		{"1/5 filled blocks", 3600 * 24 * 7, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(0.2, -2)}},
-		{"1/4 filled blocks", 3600 * 24 * 7, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(0.25, -2)}},
-		{"1/3 filled blocks", 3600 * 24 * 7, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(0.33, -2)}},
-		{"1/2 filled blocks", 3600 * 24 * 7, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(0.5, -2)}},
-
+			{"1/10 filled blocks", 3600, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(0.1, -2)}},
+			{"1/5 filled blocks", 3600 * 24 * 7, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(0.2, -2)}},
+			{"1/4 filled blocks", 3600 * 24 * 7, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(0.25, -2)}},
+			{"1/3 filled blocks", 3600 * 24 * 7, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(0.33, -2)}},
+			{"1/2 filled blocks", 3600 * 24 * 7, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(0.5, -2)}},
+		*/
 		// exceeding block gas target tests, the baseFee adjustment will start for below tests, the TXN fee will increase fast.
 
-		// over target with 10% percentage.
-		{"11/20 filled blocks", 700, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(0.55, -2)}},
+		{"over target 10%", thirtyMins, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(0.55, -2)}},
 
-		// over target with 20% percentage.
-		{"3/5 filled blocks", 360, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(0.66, -2)}},
+		{"over target 20%", thirtyMins, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(0.60, -2)}},
 
-		// over target with 50% percentage.
-		{"3/4 filled blocks", 181, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(0.75, -2)}},
+		{"over target 30%", thirtyMins, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(0.65, -2)}},
 
-		// over target with 100% percentage.
-		{"full filled blocks", 181, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(1.0, -2)}},
+		{"over target 40%", thirtyMins, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(0.70, -2)}},
+
+		{"over target 50%", thirtyMins, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(0.75, -2)}},
+
+		{"over target 100%", thirtyMins, &defSysParams, &RatedPacker{ratio: decimal.NewFromFloatWithExponent(1.0, -2)}},
 
 		// dynamic traffic
-		{"dynamic filled blocks", 181, &defSysParams, &dynamicPacker{increasingInterval: 50, decreasingInterval: 130}},
+		//{"dynamic filled blocks", 181, &defSysParams, &dynamicPacker{increasingInterval: 50, decreasingInterval: 130}},
 	}
 
 	for i, tt := range tests {
