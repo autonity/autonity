15f015e93c7f59f9c2c355e1afba6b1d2831f9b3
ScottRMalley <35613223+ScottRMalley@users.noreply.github.com>
2025-07-07 16:30:01 +0200

    Latency selection signer set vote cache (#1201)
    * initial addition of aggregator cache
    
    * refactor cache marking, fix invalid vote bug
    
    * small refactoring

--- consensus/tendermint/backend/aggregator.go
+++ consensus/tendermint/backend/aggregator.go
@@ -106,6 +106,7 @@ func newAggregator(backend interfaces.Backend, core interfaces.Core, logger log.
 		internalCoreCh:   make(chan events.MessageEventer, 1),
 		internalFdCh:     make(chan events.MessageEventer, 1),
 		signalFastTickCh: make(chan struct{}, 1),
+		signerSetCache:   newAggregatorCache(),
 	}
 }
 
@@ -136,18 +137,6 @@ func NewRoundInfo() *RoundInfo {
 	}
 }
 
-type valueCache struct {
-	signatures map[common.Hash][]uint16
-}
-
-func (vc *valueCache) Add() {
-
-}
-
-func (vc *valueCache) HaveSuperSet() bool {
-	return false
-}
-
 type aggregator struct {
 	backend interfaces.Backend
 	core    interfaces.Core
@@ -158,8 +147,9 @@ type aggregator struct {
 	messagesFrom map[common.Address][]common.Hash
 	toIgnore     map[common.Hash]struct{}
 
-	knownMessages *fixsizecache.Cache[common.Hash, bool] // the cache of self messages
-	afdDispatchCh chan<- events.MessageEventer
+	knownMessages  *fixsizecache.Cache[common.Hash, bool] // the cache of self messages
+	signerSetCache *aggregatorCache
+	afdDispatchCh  chan<- events.MessageEventer
 
 	internalCoreCh   chan events.MessageEventer
 	internalFdCh     chan events.MessageEventer
@@ -519,7 +509,6 @@ func (a *aggregator) processVotesFor(h uint64, r int64, c uint8, v common.Hash) 
 	default:
 		a.logger.Crit("Unexpected code", "c", c)
 	}
-
 }
 
 func (a *aggregator) DispatchCoreEvents() {
@@ -630,6 +619,7 @@ func (a *aggregator) processBatches(batches [][]events.UnverifiedMessageEvent, e
 			case *message.Prevote:
 				aggregateVotes := message.AggregatePrevotesSimple(validVotes)
 				for _, aggregateVote := range aggregateVotes {
+					a.signerSetCache.AddPrevote(aggregateVote)
 					a.knownMessages.Add(aggregateVote.Hash(), true) // prevents processing of the same aggregate computed by another peer
 					a.internalCoreCh <- eventer(aggregateVote, events.UnverifiedMessageEvent{Sender: a.backend.Address()}).(events.MessageEventer)
 					a.internalFdCh <- eventer(aggregateVote, events.UnverifiedMessageEvent{Sender: a.backend.Address()}).(events.MessageEventer)
@@ -637,6 +627,7 @@ func (a *aggregator) processBatches(batches [][]events.UnverifiedMessageEvent, e
 			case *message.Precommit:
 				aggregateVotes := message.AggregatePrecommitsSimple(validVotes)
 				for _, aggregateVote := range aggregateVotes {
+					a.signerSetCache.AddPrecommit(aggregateVote)
 					a.knownMessages.Add(aggregateVote.Hash(), true) // prevents processing of the same aggregate computed by another peer
 					a.internalCoreCh <- eventer(aggregateVote, events.UnverifiedMessageEvent{Sender: a.backend.Address()}).(events.MessageEventer)
 					a.internalFdCh <- eventer(aggregateVote, events.UnverifiedMessageEvent{Sender: a.backend.Address()}).(events.MessageEventer)
@@ -757,6 +748,14 @@ func (a *aggregator) handleEvent(event events.UnverifiedMessageEvent) {
 	if err != nil {
 		panic(fmt.Sprintf("cannot get committee of height: %d", msg.H()))
 	}
+
+	if a.signerSetCache.Contains(msg.H(), msg.R(), committee.Len(), event) {
+		return // already processed a message with more signers
+	} else {
+		// mark committee size for the height to avoid any more calls to CommitteeByHeight
+		a.signerSetCache.MarkCommitteeSize(msg.H(), committee.Len())
+	}
+
 	quorum := bft.Quorum(committee.TotalVotingPower())
 
 	coreRound := a.core.Round()
@@ -1026,6 +1025,7 @@ loop:
 			// cleanup
 			clear(a.messagesFrom)
 			clear(a.toIgnore)
+			a.signerSetCache.PruneToHeight(coreHeight)
 			aggTimer.Reset(aggregationPeriod)
 		case <-oldMessagesTicker.C:
 			a.logger.Trace("Processing stale messages in the aggregator")
