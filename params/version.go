af80bfbe6d06e3bf9c525a5e2f0091d5d7202821
Youssef Azzaoui <ya@clearmatics.com>
2025-07-19 04:37:32 -0300

    Bump version 1.1.0

--- params/version.go
+++ params/version.go
@@ -22,9 +22,9 @@ import (
 
 const (
 	VersionMajor = 1       // Major version component of the current release
-	VersionMinor = 0       // Minor version component of the current release
-	VersionPatch = 2       // Patch version component of the current release
-	VersionMeta  = "alpha" // Version metadata to append to the version string
+	VersionMinor = 1       // Minor version component of the current release
+	VersionPatch = 0       // Patch version component of the current release
+	VersionMeta  = "" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
