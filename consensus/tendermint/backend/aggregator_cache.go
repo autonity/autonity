5bcb4c5ca576036584606f3db6517f5aff9580ac
lorenzo <31852651+lorenzo-dev1@users.noreply.github.com>
2025-07-29 17:30:28 +0200

    revert proposal filtering

--- consensus/tendermint/backend/aggregator_cache.go
+++ consensus/tendermint/backend/aggregator_cache.go
@@ -209,7 +209,7 @@ func (c *aggregatorCache) contains(height uint64, round int64, event events.Unve
 func (c *aggregatorCache) filter(event events.UnverifiedMessageEvent) bool {
 	msg := event.Message
 	switch msg.(type) {
-	case *message.Propose, *message.Precommit, *message.Prevote:
+	case *message.Precommit, *message.Prevote:
 		received, dispatched := c.contains(msg.H(), msg.R(), event)
 		if dispatched {
 			return true
@@ -230,6 +230,8 @@ func (c *aggregatorCache) filter(event events.UnverifiedMessageEvent) bool {
 		}
 		return received && dispatched
 	default:
+		// proposals are never filtered as they are validated in the backend, and
+		// duplicated proposals should already be filtered by the backend itself.
 		return false
 	}
 }
