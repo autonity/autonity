7756ae04f851bde38d2cacbdc6ea6553c5d402c9
lorenzo <31852651+lorenzo-dev1@users.noreply.github.com>
2025-10-02 14:59:02 +0200

    ignore linting error

--- autonity/tests/base.go
+++ autonity/tests/base.go
@@ -713,7 +713,7 @@ func NewAccusationEvent(height uint64, value common.Hash, reporter common.Addres
 			FakeValidRound:     vr,
 			FakeValidRoundNil:  false,
 			FakeSigner:         offender,
-			FakeSignerIndex:    uint64(offenderIndex),
+			FakeSignerIndex:    uint64(offenderIndex), //nolint:gosec
 			FakeVerified:       true,
 		}, true)}
 	case autonity.C1:
